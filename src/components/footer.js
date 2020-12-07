758475dee6aaba35495b6cf9769c74e21ae0d92a
Bassam-azizi <101010bassam@gmail.com>
2020-12-07 23:30:04 +0100

    Create footer's content

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () =>{
+    return(
+        <div>
+            <p>contact@radiokhemir.fr</p>
+            <p>made with ♥ by Bassem Azizi</p>
+            <p>All right are reserved</p>
+        </div>
+    )
+}
+
+export default Footer
