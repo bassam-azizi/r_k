85a0c81a318d5eafa89e876558c05a45b0225a8b
Bassam-azizi <101010bassam@gmail.com>
2020-12-07 23:30:22 +0100

    create the Header 's content

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+const Header = () =>{
+    return(
+        <div>
+            <div></div>
+            <nav>
+                <ul>
+                    <li>About us</li>
+                    <li>Program</li>
+                    <li>Emission</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <div>Eng</div>
+        </div>
+    )
+}
+
+export default Header
