c3ddffa33e8208fd16043d922ac8f58f013dd95b
Bassam-azizi <101010bassam@gmail.com>
2020-12-07 21:33:37 +0100

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const IndexPage = () =>{
+  return(
+    <div>
+      <h1>Hello World</h1>
+      <p> this is a hello world page </p>
+    </div>
+  )
+}
+export default IndexPage
