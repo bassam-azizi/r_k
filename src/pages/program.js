b82173dd4e827cee1fb613c7df684395aac54e64
Bassam-azizi <101010bassam@gmail.com>
2020-12-07 23:33:15 +0100

    import the layout to include inside the program page

---
+++ src/pages/program.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+
+
+const Program = () =>{
+    return (
+        <Layout> This is the Program page</Layout>
+    )
+}
+
+export default Program
