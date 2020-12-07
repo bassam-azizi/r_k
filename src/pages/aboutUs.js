1282353b4d3d912262de7018b99a25ad8dcc0a54
Bassam-azizi <101010bassam@gmail.com>
2020-12-07 23:31:36 +0100

    import the layout component to aboutUs page

---
+++ src/pages/aboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+
+const AboutUs = () =>{
+    return(
+        <Layout>This is the about us page</Layout> 
+    )
+}
+
+export default AboutUs
