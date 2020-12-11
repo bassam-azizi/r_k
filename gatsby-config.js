1f9a81ea2a8ea60f860bc5552ee3a3ab27ae3020
Bassam-azizi <101010bassam@gmail.com>
2020-12-11 23:15:50 +0100

    gatsby-image added to the gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,14 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const path = require(`path`)
 
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-emotion`,
@@ -18,6 +22,13 @@ module.exports = {
         labelFormat: `[local]`,
         cssPropOptimization: true,
       },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      }
     },
   ],
 }
