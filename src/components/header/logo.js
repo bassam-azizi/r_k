643aee00963419f1be5e3948fac0247fda51b9be
Bassam-azizi <101010bassam@gmail.com>
2020-12-13 21:58:38 +0100

    make the radiokhemir logo clickable by importing Link component

--- src/components/header/logo.js
+++ src/components/header/logo.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Img from 'gatsby-image'
 import Styled from '@emotion/styled'
-import {useStaticQuery, graphql} from 'gatsby'
+import {Link, useStaticQuery, graphql} from 'gatsby'
 
 
-const LogoWrap = Styled.div`
+const LogoWrap = Styled(Link)`
   margin: auto 0;
-  flex: 0 1 116px;
+  /*flex: 0 1 116px; */
+  min-width: 152px;
+  min-height:104px;
   margin-bottom: 20px;
+  
 
   @media (max-width: 768px) and (orientation: landscape) {
     flex: 0 1 75px;
@@ -21,7 +24,7 @@ const Logo = () =>{
     query{
         file(name: {eq: "logo"}, extension: {eq: "png"}){
             childImageSharp{
-                fluid(maxWidth: 400, pngQuality: 80){
+                fluid(maxWidth: 200, pngQuality: 80){
                     ...GatsbyImageSharpFluid
                 }
             }
@@ -29,8 +32,8 @@ const Logo = () =>{
     }
     `)
     return(
-        <LogoWrap>
-            <Img fluid={data.file.childImageSharp.fluid} alt="Radio Khemir" />
+        <LogoWrap to='/'>
+            <Img fluid={data.file.childImageSharp.fluid} loading="eager" alt="Radio Khemir" />
         </LogoWrap>
     )
 }
