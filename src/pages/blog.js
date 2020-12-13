f4a9d4abd6d7bbf894a8418bb855c38101550b3f
Bassam-azizi <101010bassam@gmail.com>
2020-12-13 22:00:35 +0100

    create the Blog page and add it to the site

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Blog = () =>{
+    return(
+        <Layout>
+            <h1>this is the blog</h1>
+        </Layout>
+    )
+}
+
+export default Blog
