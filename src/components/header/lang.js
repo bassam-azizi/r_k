20c8a5f6b46f49bc7541b276e7e45984fa1fcea3
Bassam-azizi <101010bassam@gmail.com>
2020-12-13 21:58:00 +0100

    change the aling self to end and add margin-lef 80px to wrapper

--- src/components/header/lang.js
+++ src/components/header/lang.js
@@ -5,13 +5,16 @@ const LangWrapper = Styled.div`
     display: flex;
     flex-direction: row;
     font-size: 14px;
-    align-self: center;
+    align-self: end;
+    margin-left: 80px;
 `
 
 const LangP = Styled.p`
-    padding: .3rem;
+    padding: .3rem .4rem;
     border: 1px solid #33333320;
     cursor: pointer;
+    color: #fff;
+    background-color: #7F8A64;
 `
 
 
