77004206c86c57c5375925d13283387bd57503f8
Bassam-azizi <101010bassam@gmail.com>
2020-12-13 21:55:03 +0100

    shrink the height of the footer to 50 px

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,10 +7,13 @@ const Container = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between ;
-    padding: 2rem 2rem;
+    align-items: center;
+    height: 50px;
+    padding: 0 4.5rem;
     margin: 0;
     background-color: #373737;
     color: #FFF;
+    font-size: 14px;
 `
 const FooterP = styled.div`
     margin: 0;  
@@ -20,7 +23,7 @@ const Footer = () =>{
     return(
         <Container>
             <FooterP>contact@radiokhemir.fr</FooterP>
-            <FooterP>made with ♥ by Bassem Azizi</FooterP>
+            <FooterP>made with <span style={{color: '#C52127'}}>♥</span> by Bassem Azizi</FooterP>
             <FooterP>All right are reserved</FooterP>
         </Container>
     )
