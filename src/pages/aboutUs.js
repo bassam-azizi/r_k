5be918616491d495f9d51d787cfdbf9b1913ff6b
Bassam-azizi <101010bassam@gmail.com>
2020-12-14 19:15:06 +0100

    change the name of the aboutus component to aboutCompo

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Aboutus from '../components/aboutUs/aboutUs'
+import Aboutus from '../components/aboutUs/aboutCompo'
 
 
 const AboutUs = () =>{
