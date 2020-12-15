9b929d5df861c87ac04d75008340220cacd52c8f
Bassam-azizi <101010bassam@gmail.com>
2020-12-15 23:41:15 +0100

    add roboto to gatsby-plugin-google-fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,7 @@ module.exports = {
       options: {
         fonts: [
           `Poppins`,
-          `Roboto`,
-          `source sans pro\300,400,400i,700` // you can also specify font weights and styles
+          `Roboto`
         ],
         display: 'swap'
       }
