1d64a066783a817e9bd5e535f9b4fd5479c290d5
bassam-azizi <101010bassam@gmail.com>
2021-01-04 22:37:35 +0100

    make title background color: white

--- src/components/blog/blogPost.js
+++ src/components/blog/blogPost.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Img from 'gatsby-image'
 
 import Styled from '@emotion/styled'
 
+
 const Wrapper = Styled.div`
     padding: 0;
-    border: .5px solid #cfcfcf;
-    width: 319px;
+    border: .5px solid #fff;
+    max-width: 319px;
     height: 252px;
     position: relative;
+    margin: 138px 0 0 138px;
 `
 const Picture = Styled.picture`
     width: 100%;
@@ -22,21 +25,30 @@ const Title = Styled.div`
     bottom: 0;
     padding: 4px 1px;
     text-align: center;
-    border-top: .5px solid #cfcfcf;
+    border-top: .5px solid #fff;
+    background-color: #ffffffbb;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const H1 = Styled.h1`
+    font-size: 18px;
+    text-align: left;
+    text-indent: 3px;
+    text-align: center;
 `
 
 
-const Blogpost = () =>{
+const Blogpost = props =>{
     return(
-        <Wrapper>
-            <Picture>
-                <img src="../../images/logo.png" alt="" />
-            </Picture>
-            <Title>
-                <h1>hallo</h1>
-                <p>this is lorem*14</p>
-            </Title>
-        </Wrapper>
+            <Wrapper key={props.data.node.id}>
+                <Picture>
+                <Img fixed={props.data.node.featuredImg.childImageSharp.fixed} alt="anythings " />
+                </Picture>
+                <Title>
+                    <H1>{props.data.node.name}</H1>
+                </Title>
+            </Wrapper>
     )
 }
 
