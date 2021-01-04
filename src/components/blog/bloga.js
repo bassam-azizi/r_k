97874f3e9e68e2eaf5a711a23f2bd1458d86b7cd
bassam-azizi <101010bassam@gmail.com>
2021-01-04 22:37:10 +0100

    add space gap between news in the blog component

--- src/components/blog/bloga.js
+++ src/components/blog/bloga.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql , useStaticQuery } from 'gatsby'
+
 
 import Styled from '@emotion/styled'
 
@@ -10,13 +12,38 @@ const Wrapper = Styled.div`
     flex-wrap: wrap;
     font-family: Roboto;
     font-size: 15px;
-
+    margin: -138px 0 0 -138px;
+    width: calc(100% + 138px);
 `
 
 const Bloga = () =>{
+    const data = useStaticQuery(graphql`
+        query{
+            allStrapiBlogpost{
+                edges{
+                    node{
+                        name
+                        Slug
+                        id
+                        pubdate
+                        featuredImg{
+                            childImageSharp{
+                                fixed(width: 300, height: 250){
+                                    ...GatsbyImageSharpFixed
+                                }
+                            }
+                        }
+                        body
+                    }
+                }
+            }
+        }
+    `)
     return(
         <Wrapper>
-            <Blogpost />
+            {data.allStrapiBlogpost.edges.map(edge => 
+                <Blogpost data={edge} />
+            )}
         </Wrapper>
     )
 }
