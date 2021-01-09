5aabefd00ea636ac2e4eb1ab83b9e42097f7ce66
bassam-azizi <101010bassam@gmail.com>
2021-01-09 23:28:36 +0100

    working with the audio player

--- src/components/episodes/podcast.js
+++ src/components/episodes/podcast.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
 
 
 const Podcast = props =>{
-  const mp3 = props.data.enclosure.url
+  const mp3 = props.data.item.enclosure.url
   return(
-    <div key={props.data.key}>
-      <p>{props.data.title}</p>
-      <p>{props.data.pubDate}</p>
-      <p>{props.data.link}</p>
-      <audio controls>
-        <source src={mp3} type="audio/mp3" />
-        <track default src="captions_es.vtt" kind="captions" srcLang="en" label="spanish_captions" />
-      </audio>
-    </div>
+    <AudioPlayer src={mp3} />
+    // <div key={props.data.key}>
+    //   <p>{props.data.item.title}</p>
+    //   <p>{props.data.pubDate}</p>
+    //   <audio controls>
+    //     <source src={mp3} type="audio/mp3" />
+    //     <track default src="captions_es.vtt" kind="captions" srcLang="en" label="spanish_captions" />
+    //   </audio>
+    // </div>
   )
 }
 
