711049ea6b4c4e02372de0577dc902245ac6f538
bassam-azizi <101010bassam@gmail.com>
2021-01-09 23:29:39 +0100

    stylying the blog template content

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -8,16 +8,27 @@ import Layout from '../components/layout'
 
 
 const Container = Styled.div`
-    background-color: #cfcfcf;
     padding: 0 2rem;
     cursor: pointer;
 `
 const Header = Styled.div`
     padding: 2rem 0;
-    text-align: center;
+    // text-align: center;
+    padding: 0 3rem 3rem;
+    & > p{
+        font-size: 12px;
+        color: #888;
+    }
 `
 const Body = Styled.div`
-    color: #aaa;
+    color: #444;
+    & > p{
+        line-height: 60px;
+        // color: #888;
+        font-weight: 500;
+        padding: 0 3rem;
+        font-size: 17px;
+    }
 `
 
 const Blog_template = ({data}) =>{
@@ -26,6 +37,7 @@ const Blog_template = ({data}) =>{
             <Container>
                 <Header>
                     <h1>{data.strapiBlogpost.name}</h1>
+                    <p>{data.strapiBlogpost.pubdate}</p>
                 </Header>
                 <Body>
                     <Reactmarkdown source={data.strapiBlogpost.body} />
