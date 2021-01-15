7a255287664fa87457cdc3e67fb95df21191fbd2
bassam-azizi <101010bassam@gmail.com>
2021-01-15 23:28:49 +0100

    add the progressBar and style it

--- src/components/episodes/podcast.js
+++ src/components/episodes/podcast.js
@@ -3,9 +3,7 @@ import AudioPlayer , {RHAP_UI} from 'react-h5-audio-player'
 import 'react-h5-audio-player/lib/styles.css'
 import './podcast.scss'
 import Styled from '@emotion/styled'
-// import { FaPlay } from "react-icons/all"
 import PlayBtn from './playbtn.js'
-// import SoundBtn from './soundBtn.js'
 
 const Wrapper = Styled.div`
   width: 600px;
@@ -32,38 +30,27 @@ const Info = Styled.div`
   }
 `
 const Play = <PlayBtn />
-// const Sound = <SoundBtn />
 
 const Podcast = props =>{
   const mp3 = props.data.item.enclosure.url
-  const progressBar = RHAP_UI.PROGRESS_BAR
   return(
     <Wrapper>
       <AudioPlayer 
         src={mp3} 
         customIcons={{
            play: Play
-          //  volume: Sound
 }}
         customProgressBarSection={[
           RHAP_UI.MAIN_CONTROLS,
           RHAP_UI.CURRENT_TIME
         ]}
         // customVolumeControls={[]}
-        customControlsSection={[RHAP_UI.VOLUME_CONTROLS,<Info><h5>{props.data.item.title}</h5><p>{props.data.item.enclosure.length}</p></Info>,RHAP_UI.DURATION]}
+        customControlsSection={[RHAP_UI.VOLUME_CONTROLS,<Info><h5>{props.data.item.title}</h5><p>{props.data.item.enclosure.length}</p></Info>, RHAP_UI.PROGRESS_BAR,RHAP_UI.DURATION]}
         customAdditionalControls={[]}
         showJumpControls={false}
         layout="horizontal-reverse"
       />
     </Wrapper>
-    // <div key={props.data.key}>
-    //   <p>{props.data.item.title}</p>
-    //   <p>{props.data.pubDate}</p>
-    //   <audio controls>
-    //     <source src={mp3} type="audio/mp3" />
-    //     <track default src="captions_es.vtt" kind="captions" srcLang="en" label="spanish_captions" />
-    //   </audio>
-    // </div>
   )
 }
 
