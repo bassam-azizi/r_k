4dbc62c3628cb5e2b1e494aac20a05e2f37d6a20
bassam-azizi <101010bassam@gmail.com>
2021-01-15 23:30:38 +0100

    style the shedule pge basic style

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Reactmarkdown from 'react-markdown'
 import { graphql , useStaticQuery } from 'gatsby'
 import Styled from "@emotion/styled"
@@ -6,6 +7,13 @@ import Styled from "@emotion/styled"
 
 
 const Wrapper = Styled.div`
+    position: relative;
+    font-family: Roboto;
+    background-color: #e5e5E5 ;
+    border: 1px solid #373737 ;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     width : 400px;
     margin: 0 auto;
     padding: 4rem;
@@ -15,6 +23,23 @@ const Wrapper = Styled.div`
     & > ul > li{
         line-height: 40px;
     }
+`
+const Title = Styled.div`
+    padding: 20px 0;
+`
+const Bars = Styled.div`
+    position: absolute;
+    top: -36px;
+    height: 60px;
+    display: flex;
+`
+const Bar = Styled.div`
+    background-color: red;
+    width: 3px;
+    margin-right: 16px;
+`
+const Footer = Styled.div`
+    padding: 30px 0;
 `
 
 const Schedule = () =>{
@@ -30,8 +55,14 @@ const Schedule = () =>{
     `)
     return(
         <Wrapper>
-            <div><h1>{data.strapiSchedule.eng_day}</h1></div>
+            <Bars>
+                <Bar></Bar>
+                <Bar></Bar>
+                <Bar></Bar>
+            </Bars>
+            <Title><h1>{data.strapiSchedule.eng_day}</h1></Title>
             <Reactmarkdown source={data.strapiSchedule.eng_body} />
+            <Footer>You can watch past shows <Link to='/emission'>here</Link></Footer>
         </Wrapper>
 
     )
