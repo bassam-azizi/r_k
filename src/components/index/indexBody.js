dac5796ecb173335c3d1efb1b1374c35d8ca56c8
bassam-azizi <101010bassam@gmail.com>
2021-01-26 23:21:32 +0100

    set a margin to the index body wrapper

--- src/components/index/indexBody.js
+++ src/components/index/indexBody.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Cta from './cta/cta'
-import FeaturedNews from './featuredNews'
+import FeaturedNews from './news/featuredNews'
 import Styled from '@emotion/styled'
 
 const Wrapper = Styled.div`
     display: flex;
+    margin: 0 -5vw;
+    width: auto;
 `
 
 const IndexBody = () => {
