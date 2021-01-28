5f8b1c0eac86cada926f892c93aea7f0c678f0c4
bassam-azizi <101010bassam@gmail.com>
2021-01-28 23:54:32 +0100

    change nth-child selector to nth-of-type and create info icon and style't

--- src/components/index/news/post.js
+++ src/components/index/news/post.js
@@ -4,20 +4,24 @@ import Img from 'gatsby-image'
 import Styled from '@emotion/styled'
 
 const Deck  = Styled.div`
+    opacity: .8;
     margin: 0;
     padding: 0;
     height: 331px;
-    &:nth-child(1){
-        width: 558px;
-    } 
-    &:nth-child(2){
+    &:hover{
+        opacity: .4;
+    }
+    &:nth-of-type(1){
         width: 433px;
-    }   
-    &:nth-child(3){
-        width: 463px;
     } 
-    &:nth-child(4){
+    &:nth-of-type(2){
+        width: 558px;
+    }   
+    &:nth-of-type(3){
         width: 529px;
+    } 
+    &:nth-of-type(4){
+        width: 463px;
     }      
     Img {
         object-fit: cover;
@@ -25,11 +29,25 @@ const Deck  = Styled.div`
     .gatsby-image-wrapper{
         height: 331px;
     }
+    position: relative;
 `
 const Lin = Styled(Link)`
     text-decration: none;
     color: #222;
 `
+const Span = Styled.p`
+    border-radius: 50%;
+    padding: 4px 10px;
+    background-color: #fff;
+    color: #222;
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    font-family: Roboto;
+    font-weight: bold;
+    opacity: .6;
+`
+
 
 const Post = props =>{
     return(
@@ -37,6 +55,7 @@ const Post = props =>{
             <Lin to ={`/blog/${props.data.node.Slug}`}>
             <Img fluid={props.data.node.featuredImg.childImageSharp.fluid} alt={props.data.node.name} />
             </Lin>
+            <Span>i</Span>
         </Deck>
     )
 }
