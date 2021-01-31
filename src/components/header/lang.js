dede629698c433b58ad88619f8c4f02b52c5ae6f
bassam-azizi <101010bassam@gmail.com>
2021-02-01 00:35:29 +0100

    reduce the top/bottom padding of Lang cells and gave the red color to selected lang

--- src/components/header/lang.js
+++ src/components/header/lang.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Styled from '@emotion/styled'
 
 const LangWrapper = Styled.div`
@@ -6,16 +7,19 @@ const LangWrapper = Styled.div`
     flex-direction: row;
     font-size: 14px;
     align-self: end;
-    margin-left: 80px;
-`
-
-const LangP = Styled.p`
-    padding: .3rem .4rem;
+    margin-bottom: 19px;
+    `
+    
+    const LangP = Styled(Link)`
+    padding: .2rem .4rem;
     border: 1px solid #33333320;
     cursor: pointer;
     color: #fff;
     background-color: #7F8A64;
-`
+    &:nth-of-type(3){
+        background: #C52127;
+    }
+    `
 
 
 
