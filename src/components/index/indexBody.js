273700c824341d4e772b509e909d903eaaa629e4
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:49:31 +0100

    :) separate styled component from logic component

--- src/components/index/indexBody.js
+++ src/components/index/indexBody.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Cta from './cta/cta'
-import FeaturedNews from './news/featuredNews'
+import Posts_Collection from './posts/featuredPosts'
 import ProgramBar from './programBar'
 import Styled from '@emotion/styled'
 
 const Wrapper = Styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: flex-end;
 `
 const Container = Styled.div`
     display: flex;
-    margin-top: 64px;
+    // margin-top: 64px;
     justify-content: flex-end;
 `
 
@@ -19,7 +20,7 @@ const IndexBody = () => {
         <Wrapper>
             <Container>
                 <Cta />
-                <FeaturedNews />
+                <Posts_Collection />
             </Container>
             <ProgramBar />
         </Wrapper>
