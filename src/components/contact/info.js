670ec15857cf9c10d623a15d9b10b2598ef41254
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:47:12 +0100

    :) separate styled component from logic component

--- src/components/contact/info.js
+++ src/components/contact/info.js
@@ -1,32 +1,5 @@
 import React from 'react'
-
-import Styled from '@emotion/styled'
-
-
-const Wrapper = Styled.div`
-    padding: 1.5em;
-    background: #373737;
-    color: #fff;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    min-height: 100%;
-`
-const Adress = Styled.div`
-    color: f1f1f1;
-    flex-grow: 1;
-`
-const H1 = Styled.h1`
-    font-weight: 600;
-`
-const Sponsored = Styled.div`
-    font-style: italic;
-    margin-bottom: 2px; 
-    `
-
+import { Wrapper, Adress, H1, Sponsored} from '@emotion/styled'
 
 
 const Info = () =>{
