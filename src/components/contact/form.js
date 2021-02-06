4135e5e6c63d77bb08f0f5859ba32257f77c8e7b
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:46:56 +0100

    :) separate styled component from logic component

--- src/components/contact/form.js
+++ src/components/contact/form.js
@@ -1,52 +1,5 @@
 import React from 'react'
-
-import Styled from '@emotion/styled'
-
-
-
-const Wrapper = Styled.form`
-    padding: 2em;
-    background-color: #E5E5E5 ;
-    width: 50%;
-
-`
-const H1 = Styled.h1`
-    color: #505D2E;
-    font-size: 24px;
-`
-const Input_container = Styled.div`
-    width: 75%;
-    position: relative;
-    margin: 45px auto -25px;
-`
-const Input = Styled.input`
-    width: 100%;
-    padding: 0.1rem;
-`
-const Btn = Styled.input`
-    background-color: #C52127;
-    border-radius: 10px;
-    color: #fff;
-    width: 100%;
-    padding: 0.1rem;
-    line-height: 3;
-    font-size: 16px;
-    font-weight: 400;
-    cursor: pointer;
-`
-const Label = Styled.label`
-    position: absolute;
-    left: 7px;
-    font-weight: 300;
-    color: #444;
-    font-size: 15px;
-    top: 0.4rem;
-`
-
-const Textarea = Styled.textarea`
-    overflow: hidden;
-    width: 100%;
-` 
+import {Wrapper, H1, Input_container, Input, Btn, Label, Textarea} from './formStyle'
 
 const Form = () =>{
     return(
