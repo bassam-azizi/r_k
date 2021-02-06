6aa2047f64ff82329ec3de5dd2078b9d53c9f883
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:50:31 +0100

    :) separate styled component from logic component

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
-import Aboutus from '../components/aboutUs/aboutC'
+import Aboutus from '../components/aboutUs/aboutBody'
 
 
 const AboutUs = () =>{
