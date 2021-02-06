46fc8f223a8c61bd30cb5b5970cb993259e13240
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:44:07 +0100

    :) separate styled component from logic component

---
+++ src/components/footerStyle.js
@@ -0,0 +1,22 @@
+import Styled from '@styled/emotion'
+
+
+export const Container = Styled.div`
+    display: block;
+    height: 60px;
+    padding: 7px 4.5rem;
+    margin: 0;
+    background-color: #373737;
+    color: #FFF;
+    font-size: 14px;
+    flex-shrink:0;
+    div{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+    }
+`
+export const P = Styled.div`
+    margin: 0;  
+`
