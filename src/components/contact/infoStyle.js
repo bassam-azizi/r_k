e0a6b79e4ea7b4157428c7bc40a8b25b6d591e82
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:47:18 +0100

    :) separate styled component from logic component

---
+++ src/components/contact/infoStyle.js
@@ -0,0 +1,25 @@
+import Styled from '@emotion/styled'
+
+export const Wrapper = Styled.div`
+padding: 1.5em;
+background: #373737;
+color: #fff;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+width: 50%;
+display: flex;
+flex-direction: column;
+min-height: 100%;
+`
+export const Adress = Styled.div`
+color: f1f1f1;
+flex-grow: 1;
+`
+export const H1 = Styled.h1`
+font-weight: 600;
+`
+export const Sponsored = Styled.div`
+font-style: italic;
+margin-bottom: 2px; 
+`
