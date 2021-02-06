96755a1405eb0ab2965d94578bf87d3650fb6267
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:46:28 +0100

    :) separate styled component from logic component

---
+++ src/components/contact/contactBody.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Styled from '@emotion/styled'
+
+import Form from './form'
+import Info from './info'
+
+const Wrapper = Styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    border: 1px solid #000;
+`
+
+
+const ContactC = () =>{
+    return(
+    <Wrapper>
+        <Info />
+        <Form />
+    </Wrapper>
+
+    )
+}
+
+export default ContactC
