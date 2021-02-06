106b3503a6c578dec7f404e1cded21d4da320def
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:50:12 +0100

    :) separate styled component from logic component

---
+++ src/components/index/posts/post.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Link} from 'gatsby'
+import Img from 'gatsby-image'
+import { Deck , Title} from './postStyle'
+
+
+const Post = props =>{
+    return(
+        <Deck>
+            <Lin to ={`/blog/${props.data.node.Slug}`}>
+            <Img fluid={props.data.node.featuredImg.childImageSharp.fluid} alt={props.data.node.name} />
+            </Lin>
+            <Info>
+                <span>i</span>
+                <Link to={`/blog/${props.data.node.Slug}`}>
+                    <h4>{props.data.node.name}</h4>
+                    <p>{props.data.node.pubdate}</p>
+                </Link>
+            </Info>
+            <Title>
+                <p>{props.data.node.name}</p>
+            </Title>
+        </Deck>
+    )
+}
+
+export default Post
