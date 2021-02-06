08dddf7b0c27e098204ce7e865187f409da82724
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:46:11 +0100

    :) separate styled component from logic component

--- src/components/blog/blogPost.js
+++ src/components/blog/blogPost.js
@@ -1,47 +1,10 @@
 import React from 'react'
 import Img from 'gatsby-image'
-import { Link } from 'gatsby'
-
-import Styled from '@emotion/styled'
+import {Wrapper, Picture, Title, H1, Lin} from './blogPostStyle'
 
 
-const Wrapper = Styled.div`
-    padding: 0;
-    border: .5px solid #fff;
-    max-width: 319px;
-    height: 252px;
-    position: relative;
-    margin: 138px 0 0 138px;
-`
-const Picture = Styled.picture`
-    width: 100%;
-    margin: 0;
-    height: auto;
-`
-const Title = Styled.div`
-    width: 100%;
-    height: 73px;
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    padding: 4px 1px;
-    text-align: center;
-    border-top: .5px solid #fff;
-    background-color: #ffffff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-const H1 = Styled.h1`
-    font-size: 18px;
-    text-align: left;
-    text-indent: 3px;
-    text-align: center;
-`
-const Lin = Styled(Link)`
-    text-decoration: none;
-    color: #222;
-` 
+
+
 
 const Blogpost = props =>{
     return(
