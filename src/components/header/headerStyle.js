7fa8583b9978ff69ca481ce911b015a9da256f88
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:49:10 +0100

    :) separate styled component from logic component

---
+++ src/components/header/headerStyle.js
@@ -0,0 +1,30 @@
+import Styled from '@emotion/styled'
+
+export const Wrapper = Styled.nav`
+        width: 100%;
+        `
+export const Container = Styled.div`
+        max-width: 1320px;
+        margin: 28px auto;
+        height: 104px;
+        display: flex;
+        justify-content: space-between;
+        position: relative;
+        align-items: center;
+        `
+export const Hamburger = Styled.div`
+        position: fixed;
+        top: 2em;
+        right: 2em;
+        display:  none;
+        background-color: #111;
+        width: 30px;
+        height: 3px;
+        transition: all .3s linear;
+        align-self: center;
+        position: relative;
+        transform: ${props => (props.open ? "rotate(-45deg)" : "inherit")};
+        @media (max-width: 768px){
+        display: block;
+        }
+        `
