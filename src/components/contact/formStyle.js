8f9fb36350f937f803775a2fc7863d3350472d48
bassam-azizi <101010bassam@gmail.com>
2021-02-07 00:47:05 +0100

    :) separate styled component from logic component

---
+++ src/components/contact/formStyle.js
@@ -0,0 +1,45 @@
+import Styled from '@emotion/styled'
+
+export const Wrapper = Styled.form`
+    padding: 2em;
+    background-color: #E5E5E5 ;
+    width: 50%;
+
+`
+export const H1 = Styled.h1`
+    color: #505D2E;
+    font-size: 24px;
+    `
+export const Input_container = Styled.div`
+    width: 75%;
+    position: relative;
+    margin: 45px auto -25px;
+    `
+export const Input = Styled.input`
+    width: 100%;
+    padding: 0.1rem;
+    `
+export const Btn = Styled.input`
+    background-color: #C52127;
+    border-radius: 10px;
+    color: #fff;
+    width: 100%;
+    padding: 0.1rem;
+    line-height: 3;
+    font-size: 16px;
+    font-weight: 400;
+    cursor: pointer;
+    `
+export const Label = Styled.label`
+    position: absolute;
+    left: 7px;
+    font-weight: 300;
+    color: #444;
+    font-size: 15px;
+    top: 0.4rem;
+    `
+
+export const Textarea = Styled.textarea`
+    overflow: hidden;
+    width: 100%;
+    ` 
