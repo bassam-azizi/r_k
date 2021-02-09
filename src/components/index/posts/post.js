ada31003d73b451462ddbf5d628b08ea7df30249
bassam-azizi <101010bassam@gmail.com>
2021-02-10 00:45:18 +0100

    :D add Lin in the style components import

--- src/components/index/posts/post.js
+++ src/components/index/posts/post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import Img from 'gatsby-image'
-import { Deck , Title} from './postStyle'
+import { Deck , Title, Lin, Info} from './postStyle'
 
 
 const Post = props =>{
