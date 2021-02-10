f7fb161bcf7de14bcec8db6a1f48dc8563d7b5b8
bassam-azizi <101010bassam@gmail.com>
2021-02-11 00:13:53 +0100

    :) create lang drop down menu

---
+++ src/components/header/lang_menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Wrapper, Close, Lin} from './style_lang_menu'
+
+
+const Lang_menu = () =>{
+    return(
+        <Wrapper>
+            <Close>^</Close>
+            <Lin>English</Lin>
+            <Lin>Francais</Lin>
+            <Lin>العربية</Lin>
+        </Wrapper>
+    )
+}
+
+
+export default Lang_menu
