c2de383a8cc0c96d1df0a479d77bce2868c72af1
bassam-azizi <101010bassam@gmail.com>
2021-02-13 06:03:43 +0100

    :) change the flex direction to column in creen 916px

--- src/components/header/headerStyle.js
+++ src/components/header/headerStyle.js
@@ -11,12 +11,13 @@ export const Container = Styled.div`
         justify-content: space-between;
         position: relative;
         align-items: center;
+
         @media (max-width: 1188px){
                 padding : 0 20px;
         }
-        @media (max-width: 906px){
+        @media (max-width: 916px){
                 flex-direction: column;
-                justify-content: flex-start;
+                align-items: flex-start;
   }
         `
 export const Hamburger = Styled.div`
