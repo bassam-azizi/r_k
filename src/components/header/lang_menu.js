f75326a182f74d04cf905f147420bb93a64715d0
bassam-azizi <101010bassam@gmail.com>
2021-02-16 06:11:05 +0100

    :smiley: pass the props data through component for styled component styling

--- src/components/header/lang_menu.js
+++ src/components/header/lang_menu.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Wrapper, Close, Lin} from './style_lang_menu'
 
 
-const Lang_menu = () =>{
+const Lang_menu = props =>{
     return(
-        <Wrapper>
-            <Close>^</Close>
+        <Wrapper menu_open={props.menu_open}>
+            <Close onClick={props.toggle}>^</Close>
             <Lin to="/">English</Lin>
             <Lin to="/">Francais</Lin>
             <Lin to="/">العربية</Lin>
