b21bc2a4ed54514440e90caceb01bb18d7165bac
bassam-azizi <101010bassam@gmail.com>
2021-02-18 03:55:07 +0100

    :smiley: restructure style selectors and wrap the second p with a div wrapper

--- src/components/aboutUs/aboutBody.js
+++ src/components/aboutUs/aboutBody.js
@@ -1,34 +1,35 @@
 import React from 'react'
-import { Wrapper , LeftColumn, H1, Li, Separator } from './aboutStyle'
+import { Wrapper , H1, Li } from './aboutStyle'
 
 
 
 const Aboutus = () =>{
     return(
         <Wrapper>
-            <LeftColumn>
-                <H1>Radio Khemir</H1>
+            <div className="about">
+                <h1 className="title">Radio Khemir</h1>
                 <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print,
                     graphic or web designs. The passage is attributed
                     to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum 
                     et Malorum for use in a type specimen book.</p>
-                <H1>Association Femme pour development</H1>
-                <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
-                    The passage is attributed to an unknown typesetter in the 
-                    15th century who is thought to have scrambled parts 
-                    of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
-            </LeftColumn>
-            <Separator />
-            <div>
-                <H1>Our Team</H1>
+                <div className="association">       
+                    <h1 className="title">Association Femme pour development</h1>
+                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
+                        The passage is attributed to an unknown typesetter in the 
+                        15th century who is thought to have scrambled parts 
+                        of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
+                </div>
+            </div>
+            <div className='team'>
+                <h1 className="title">Our Team</h1>
                 <ul>
-                    <Li>Henia Ochi : President et CEO</Li>
-                    <Li>ٌRajaa Homrani : Assistant et Animateur</Li>
-                    <Li>Rami Zaghdoudi : Animateur et DDJ</Li>
-                    <Li>Ilhem boughnaia : Animateur</Li>
-                    <Li>Hend Azizi : Animateur</Li>
-                    <Li>Amina Trabelsi : Animateur</Li>
-                    <Li>Fedi Arfaoui : Animateur et Coach</Li>
+                    <li>Henia Ochi : President et CEO</li>
+                    <li>ٌRajaa Homrani : Assistant et Animateur</li>
+                    <li>Rami Zaghdoudi : Animateur et DDJ</li>
+                    <li>Ilhem boughnaia : Animateur</li>
+                    <li>Hend Azizi : Animateur</li>
+                    <li>Amina Trabelsi : Animateur</li>
+                    <li>Fedi Arfaoui : Animateur et Coach</li>
                 </ul>
             </div>
         </Wrapper>
