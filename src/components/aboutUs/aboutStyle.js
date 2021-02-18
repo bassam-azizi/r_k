866d977d37a65b9cc7e5155df82f01c5031b28a5
bassam-azizi <101010bassam@gmail.com>
2021-02-18 03:56:44 +0100

    :satisfied: style the aboutus page and make it responsive in all screen

--- src/components/aboutUs/aboutStyle.js
+++ src/components/aboutUs/aboutStyle.js
@@ -4,30 +4,63 @@ import Styled from '@emotion/styled'
 export const Wrapper = Styled.div`
     display: flex;
     flex-direction: row;
-    padding: 6px;
-`
-export const LeftColumn = Styled.div`
-    width: 65%;
-    p{
-        letter-spacing: 0.2px;
-        line-height: 2.5;
-        padding: 0 19px; 
+    width: 90%;
+    max-width:1220px;
+    margin: 115px auto;
+    padding-left: 70px;
+
+    @media (max-width:1064px){
+        flex-direction: column;
+        margin-top: 160px;
     }
-    padding: 7px;
+
+    @media (max-width: 600px){
+        padding-left: 10px;
+    }
+
+
+    .about{
+        width: 65%;
+        p{
+            letter-spacing: 0.2px;
+            line-height: 3;
+            color: #222;
+            width: 90%;
+            margin: 0 auto;
+        } 
+
+        @media (max-width: 1064px){
+            width: 100%;
+            border-bottom: 1px solid #22222245 ;
+            padding-bottom: 30px;
+        } 
+    }
+
+    .association{
+        margin-top: 30px;
+    }
+
+    .team{
+        border-left: 1px solid #22222245 ;
+        padding-left: 10px;
+
+        ul li{
+            line-height: 3;
+            letter-spacing: 0.2px;
+            padding-left: 30px;
+            color: #222;
+        }
+
+        @media (max-width: 1064px){
+            border-left: none;
+            padding-top: 30px;
+        }
+    }
+    .title{
+        line-height: 2;
+        font-weight: 500;
+        padding-left: 12px;    
+        font-size: 20px;
+    }
 `
-export const H1 = Styled.h1`
-    line-height: 3;
-    font-weight: 400;
-    padding-left: 12px;
-`
-export const Li = Styled.li`
-    line-height: 4;
-    letter-spacing: 0.2px;
-    padding-left: 4rem;
-`
-export const Separator = Styled.div`
-    width: 1px;
-    height: 100vh;
-    background-color: #c1c1c1;
-    align-self: center;
-`
+
