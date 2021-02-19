9726414821232abe00b3280232ba50424fa5766d
bassam-azizi <101010bassam@gmail.com>
2021-02-19 06:21:09 +0100

    :satisfied: plan and design a structured style base of the episode page to fit all kind of screens

---
+++ src/components/episodes/episodeBodyStyle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styled from '@emotion/styled'
+
+
+export const Wrapper = Styled.div`
+	margin: 109px auto 30px;
+	width: 1291px;
+
+	a{
+		text-decoration: none;
+		color: #222;
+		font-size: 16px;
+		margin: 0 96px 0 17px;
+		outline: none;
+	}
+`
