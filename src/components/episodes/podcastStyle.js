5a3dffa337c03f0c0b2a2a39a97012c566f5911d
bassam-azizi <101010bassam@gmail.com>
2021-03-01 06:14:10 +0100

    style the podcast with styled component podcast info included

--- src/components/episodes/podcastStyle.js
+++ src/components/episodes/podcastStyle.js
@@ -2,26 +2,39 @@ import Styled from '@emotion/styled'
 
 export const Wrapper = Styled.div`
     width: 600px;
-        *{
-        outline: none;
-        }
+    *{
+    outline: none;
+    }
     background-color:  #aaa;
     margin: 48px 0;
+
+    @media (max-width: 1330px){
+        width: 300px;
+    }
 `
 export const Info = Styled.div`
     position: relative;
     margin-left: 15px;
     text-align: left;
+    width: 200px;
+    overflow: hidden;
         h5{
             line-height: 18.75px;
             font-weight: 400;
             font-size: 16px;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
         }
         p{
-        position: absolute;
-        font-weight: 200px;
-        font-size: 12px;
-        line-height: 14px;
-        color: #919191;
-        }   
-`
+            position: absolute;
+            font-weight: 200px;
+            font-size: 12px;
+            line-height: 14px;
+            color: #919191;
+        }  
+
+        @media (max-width: 1300px){
+            width: 60%;
+        } 
+ `
