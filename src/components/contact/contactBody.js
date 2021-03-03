46b3c463923bfad536ab2de010571680f6e02b1f
bassam-azizi <101010bassam@gmail.com>
2021-03-03 06:15:21 +0100

    :smile: make the width 80%, margin 115px auto and set style for all screen size

--- src/components/contact/contactBody.js
+++ src/components/contact/contactBody.js
@@ -8,16 +8,28 @@ import Info from './info'
 const Wrapper = Styled.div`
     display: flex;
     flex-direction: row-reverse;
-    border: 1px solid #000;
+    width: 80%;
+    margin: 115px auto;
+
+    @media (max-width: 1232px){
+        width: 90%;
+    }
+
+    @media (max-width: 900px){
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-around;
+        margin-top: 170px;
+    }
 `
 
 
 const Contact_Body = () =>{
     return(
-    <Wrapper>
-        <Info />
-        <Form />
-    </Wrapper>
+        <Wrapper>
+            <Info />
+            <Form />
+        </Wrapper>
 
     )
 }
