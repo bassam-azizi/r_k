d60fb9b058b291da670d0bdec9426af10523d529
bassam-azizi <101010bassam@gmail.com>
2021-03-13 04:17:45 +0100

    :satisifed: import The Head to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Head from '../components/head'
 
 import Contact_Body from '../components/contact/contactBody'
 
 const Contact = () =>{
     return(
         <Layout>
+        	<Head title="Contact us" />
             <Contact_Body />
         </Layout>
     )
