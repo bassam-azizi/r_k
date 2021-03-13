647f9bbe114cda67f7e9f568e426cda5af8c6442
bassam-azizi <101010bassam@gmail.com>
2021-03-13 04:17:19 +0100

    :satisifed: import The Head to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Head from '../components/head'
+
 
 import Bloga from '../components/blog/blogBody'
 
 const Blog = () =>{
     return(
         <Layout>
+        	<Head title="Press" />
             <Bloga />
         </Layout>
     )
