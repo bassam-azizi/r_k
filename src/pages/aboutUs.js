18918e6ed4af92a406bb9bdf1692fece8f67b1d2
bassam-azizi <101010bassam@gmail.com>
2021-03-13 04:16:32 +0100

    :satisifed: import The Head to aboutus page

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Head from '../components/head'
+
 import Aboutus from '../components/aboutUs/aboutBody'
 
 
 const AboutUs = () =>{
     return(
         <Layout>
+        	<Head title="About us" />
             <Aboutus />
         </Layout> 
     )
