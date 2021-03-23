75355560e063de670ee150cf7ef7eddc511fc3b6
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:38:32 +0100

    :satisifed: create the social-media links component

---
+++ src/components/contact/socialMedia.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import SocialMediaButtons from 'react-social-media-buttons';
+ 
+const Social_Media = () => {
+
+  const buttonStyle = {
+    width: '73px', 
+    height: '73px',
+     margin: '0px 16px', 
+     backgroundColor: '#ffffff'
+   };
+ 
+  const iconStyle = { color: '#373737' };
+ 
+  return(
+    <div>
+      <SocialMediaButtons 
+        links=
+        {['https://www.facebook.com/radioKhemir','https://www.instagram.com/radiokhemir/','https://twitter.com/','https://www.linkedin.com/']}
+        buttonStyle={buttonStyle} 
+        iconStyle={iconStyle}   
+        openNewTab={true}/>
+    </div>
+  );
+}
+
+  export default Social_Media
