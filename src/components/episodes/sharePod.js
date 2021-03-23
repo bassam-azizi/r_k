1cde9caaa442dc1e156bcf9ef051fceb66f08eb1
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:23:18 +0100

    :bowtie: applying the reusable component concept and styled the sharePod on that

--- src/components/episodes/sharePod.js
+++ src/components/episodes/sharePod.js
@@ -1,46 +1,30 @@
 import React from 'react'
-import { useStaticQuery, graphql} from 'gatsby'
+import Social_Share from '../../utils/socialShare'
+import { useStaticQuery, graphql } from 'gatsby'
 // import { Link } from 'gatsby'
-import {FacebookShareButton, FacebookMessengerShareButton, TwitterShareButton} from "react-share";
-import { FiFacebook, RiMessengerLine, FiTwitter } from 'react-icons/all'
 import { Wrapper } from './sharePodStyle'
 
-import Head from '../head'
 
 
 
 
-const Share_Podcast = ({props}) =>{
-  const data = useStaticQuery(graphql`
-    query{
-      site{
-        siteMetadata{
-          emissionUrl
-        }
-      }
-    }
-    `)
-	return(
-  		<Wrapper>
-    			<p>Share</p>
-          <Head title="past emission"></Head>
-    			<FacebookShareButton 
-                      url={data.site.siteMetadata.emissionUrl}
-    	                quote={`Radiokhemir - ${props}`}
-    	                hashtag={`#${props}`}
-    	                className='fb'>
-                     <FiFacebook size={26}/>
-          </FacebookShareButton>
-          
-          <TwitterShareButton 
-                  		url={'ttp://www.camperstribe.com'}
-                  		title={'ttp://www.camperstribe.com'} 
-                  		hashtags={'ain draham'}>
-                  	<FiTwitter size={26} />
-          </TwitterShareButton>
-  		</Wrapper>
+const Share_Podcast = ({ title }) => {
+  const data = useStaticQuery(graphql `
+          query{
+            site{
+              siteMetadata{
+                emissionUrl
+              }
+            }
+          }
+          `)
 
-		)
+    return (
+        <Wrapper>
+          <Social_Share title={title} path={data.site.siteMetadata.emissionUrl}/>
+        </Wrapper>
+
+    )
 }
 
 
