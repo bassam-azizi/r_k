8b22251aca10efb978673aadc353d795b1e1bad3
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:19:30 +0100

    :bowtie: add social-media pages links component to contact page

--- src/components/contact/contactBody.js
+++ src/components/contact/contactBody.js
@@ -4,22 +4,30 @@ import Styled from '@emotion/styled'
 
 import Form from './form'
 import Info from './info'
+import Social_Media from './socialMedia'
 
 const Wrapper = Styled.div`
-    display: flex;
-    flex-direction: row-reverse;
-    width: 80%;
-    margin: 115px auto;
 
-    @media (max-width: 1232px){
-        width: 90%;
+    .container{
+        display: flex;
+        flex-direction: row-reverse;
+        width: 80%;
+        margin: 115px auto;
+
+        @media (max-width: 1232px){
+            width: 90%;
+        }
+
+        @media (max-width: 900px){
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-around;
+            margin-top: 170px;
+        }
     }
-
-    @media (max-width: 900px){
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-around;
-        margin-top: 170px;
+    .smWrapper{
+        width: 420px;
+        margin: 0 auto 22px;
     }
 `
 
@@ -27,8 +35,13 @@ const Wrapper = Styled.div`
 const Contact_Body = () =>{
     return(
         <Wrapper>
-            <Info />
-            <Form />
+            <div className="container">
+                <Info />
+                <Form />
+            </div>
+            <div className="smWrapper">
+                <Social_Media />
+            </div>
         </Wrapper>
 
     )
