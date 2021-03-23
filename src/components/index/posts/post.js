ff21a9d48b26e35c5ad687221e3871c6d26a97b1
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:33:45 +0100

    :satisifed: :satisifed: restucturing and cleaning

--- src/components/index/posts/post.js
+++ src/components/index/posts/post.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import Img from 'gatsby-image'
-import { Deck , Title, Lin, Info} from './postStyle'
+import { Deck } from './postStyle'
 
 
 const Post = props =>{
     return(
         <Deck>
-            <Lin to ={`/blog/${props.data.node.Slug}`}>
+            <Link className="postLink" to ={`/blog/${props.data.node.Slug}`}>
                 <Img fluid={props.data.node.featuredImg.childImageSharp.fluid} alt={props.data.node.name} />
-            </Lin>
-            <Info>
+            </Link>
+            <div className='info'>
                 <span>i</span>
-                <Link to={`/blog/${props.data.node.Slug}`}>
+                <Link className="infoContent" to={`/blog/${props.data.node.Slug}`}>
                     <h4>{props.data.node.name}</h4>
                     <p>{props.data.node.pubdate}</p>
                 </Link>
-            </Info>
-            <Title>
+            </div>
+            <div className="title">
                 <p>{props.data.node.name}</p>
-            </Title>
+            </div>
         </Deck>
     )
 }
