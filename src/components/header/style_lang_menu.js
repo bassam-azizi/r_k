1457101a31050ed6bf2bf3587548e901ba50d4b3
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:30:26 +0100

    :satisifed: restucturing and cleaning

--- src/components/header/style_lang_menu.js
+++ src/components/header/style_lang_menu.js
@@ -1,7 +1,7 @@
 import Styled from '@emotion/styled'
 import { Link } from 'gatsby'
 
-export const Wrapper = Styled.div`
+export const Wrapper = Styled.div `
     background: #373737;
     display: block;
     width: 188px;
@@ -14,7 +14,7 @@ export const Wrapper = Styled.div`
     transform: ${ (props) => ( props.menu_open ? 'translateY(0)' : 'translateY(-700px)' )};
     transition: transform 1s ease;
 `
-export const Close = Styled.span`
+export const Close = Styled.span `
     display: block;
     font-size: 26px;
     color:#fff;
@@ -28,7 +28,8 @@ export const Close = Styled.span`
     }
     cursor: pointer;
 `
-export const Lin = Styled(Link)`
+export const Lin = Styled(Link)
+`
     font-size: 24px;
     display: block;
     padding: 41px 0;
