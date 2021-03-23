da18b68d7a7961f4213234af3979bc15a36a84ff
bassam-azizi <101010bassam@gmail.com>
2021-03-23 06:36:43 +0100

    :satisifed: add social media share and head helmet to the blog template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import Reactmarkdown from "react-markdown"
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import {Wrapper} from './styleBlogTemplate'
+import { Wrapper } from './styleBlogTemplate'
+import Share_Blog from './shareBlog'
+import Head from "../components/head"
 
 
 import Layout from '../components/layout'
 
 
-const Blog_template = ({data}) =>{
-    return(
+const Blog_template = ({ data }) => {
+    const img = process.env.PUBLIC_URL + data.strapiBlogpost.featuredImg.childImageSharp.resize.src;
+    return (
         <Layout>
+            <Head title={data.strapiBlogpost.name} image={img} path={data.site.siteMetadata.blogUrl}/>
             <Wrapper>
-                <Link className='shareBtn' to='/'>Share</Link>
-                <div className='header'>
-                    <h1>{data.strapiBlogpost.name}</h1>
-                    <p className="pubdate">{data.strapiBlogpost.pubdate}</p>
+                <div className="container">
+                    <div className='header'>
+                        <h1>{data.strapiBlogpost.name}</h1>
+                        <p className="pubdate">{data.strapiBlogpost.pubdate}</p>
+                    </div>
+                    <div className="body">
+                        <Img className='blogImg' fluid={data.strapiBlogpost.featuredImg.childImageSharp.fluid} alt='blog title' />
+                        <Reactmarkdown source={data.strapiBlogpost.body} />
+                    </div>
                 </div>
-                <div className="body">
-                    <Img className='blogImg' fluid={data.strapiBlogpost.featuredImg.childImageSharp.fluid} alt='blog title' />
-                    <Reactmarkdown source={data.strapiBlogpost.body} />
-                </div>
+                <Share_Blog title={data.strapiBlogpost.name} img={data.strapiBlogpost.featuredImg.childImageSharp.fluid}/>
             </Wrapper>
         </Layout>
     )
@@ -29,7 +35,7 @@ const Blog_template = ({data}) =>{
 export default Blog_template
 
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
         query ($slug:String!) {
             strapiBlogpost( Slug : { eq: $slug}){
                 name
@@ -43,8 +49,18 @@ export const pageQuery = graphql`
                                 fluid(maxWidth: 764){
                                     ...GatsbyImageSharpFluid
                                 }
+                                resize(width:900, quality:90){
+                                    src
+                                }
                             }
+                            publicURL
                         }
+            }
+            site{
+                siteMetadata{
+                    blogUrl
+                    url
+                }
             }
         }
 `
