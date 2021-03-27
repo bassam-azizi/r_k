f5b2f0d9bcff88d0de424cdcf899760765f8100f
bassam-azizi <101010bassam@gmail.com>
2021-03-27 07:01:31 +0100

    :bowtie: make the share blog component stick to under the article

--- src/templates/styleShareBlog.js
+++ src/templates/styleShareBlog.js
@@ -4,15 +4,31 @@ export const Wrapper = Styled.div `
 	position: fixed;
 	right: 3%;
 	top:35%;
+
+	@media (max-width: 768px){
+		position: static;
+		display: flex;
+		justify-content: center;
+	}
 	.SmContainer{
 		display: flex;
 		flex-direction: column;
+
+		@media (max-width: 768px){
+			flex-direction: row;
+			margin-bottom: 70px;
+		}
 		button{
 			margin-top: 15px;
 			opacity: .5;
+
+			@media (max-width: 768px){
+				margin-right: 15px;
+			}
 			:hover{
 				opacity: 1;
 			}
 		}
+
 	}
 `
