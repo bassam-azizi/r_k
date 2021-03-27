adbf43c7d41ddebd123807d98e55f467628f4990
bassam-azizi <101010bassam@gmail.com>
2021-03-27 07:00:38 +0100

    :bowtie: make the blog page template responsive

--- src/templates/styleBlogTemplate.js
+++ src/templates/styleBlogTemplate.js
@@ -10,16 +10,27 @@ export const Wrapper = Styled.div `
 		max-width: 770px;
 		padding: 94px 45px;
 		margin: 0 auto;
-		.shareBtn{
-			text-decoration: none;
-			color: #222;
-			position: absolute;
-			top: 18px;
-			right: 22px;
-			:hover{
-				color: #7F8A64;
-			}
+
+		@media (max-width:800px){
+			width: 80%;
 		}
+		@media (max-width: 768px){
+			padding-bottom: 45px;
+			width: 95%;
+		}
+		@media (max-width: 480px){
+			width: 100%;
+		}
+	.shareBtn{
+		text-decoration: none;
+		color: #222;
+		position: absolute;
+		top: 18px;
+		right: 22px;
+		:hover{
+			color: #7F8A64;
+		}
+	}
 	}
 
 	.header{
