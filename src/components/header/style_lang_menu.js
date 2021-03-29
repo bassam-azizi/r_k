60d1b41f634c1e9e6241dbd89622e48738285832
bassam-azizi <101010bassam@gmail.com>
2021-03-29 07:40:37 +0200

    :satisfied: change bg color of menu, remove roboto, minimise padding

--- src/components/header/style_lang_menu.js
+++ src/components/header/style_lang_menu.js
@@ -2,11 +2,10 @@ import Styled from '@emotion/styled'
 import { Link } from 'gatsby'
 
 export const Wrapper = Styled.div `
-    background: #373737;
+    background: rgb(128, 138, 102);
     display: block;
     width: 188px;
     color: #fff;
-    font-family: Roboto;
     position: absolute;
     right: 0;
     top: 0;
@@ -16,14 +15,15 @@ export const Wrapper = Styled.div `
 `
 export const Close = Styled.span `
     display: block;
-    font-size: 26px;
+    font-size: 22px;
     color:#fff;
     width: 100%;
     text-align: center;
     padding: 20px 0 10px;
     font-weight: lighter;
     :hover{
-        color: #c52127;
+        color: #8B656D;
+        // color: #c52127;
         background: #303030ee;
     }
     cursor: pointer;
@@ -32,15 +32,15 @@ export const Lin = Styled(Link)
 `
     font-size: 24px;
     display: block;
-    padding: 41px 0;
+    padding: 27px 0;
     text-align: center;
     cursor: pointer;
     text-decoration: none;
     color: #fff;
     border-bottom: 1px solid #cfcfcf12;
     :hover{
-        color: #519657 ;
-        background: #303030ee;
+        color: #fff ;
+        background: #2E3224;
         opacity: .7;
     }
     :focus{
