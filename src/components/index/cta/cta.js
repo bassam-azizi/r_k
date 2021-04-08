8bc32997d8cf70b7b603c07b8ead86913929e436
bassam-azizi <101010bassam@gmail.com>
2021-04-08 05:50:16 +0200

    :bowtie: connect the data dynamically to the translated langauge data source through intl pre-function

--- src/components/index/cta/cta.js
+++ src/components/index/cta/cta.js
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
 import Liveplayer from './livePlayer'
 import { P , Pauseicon , Icon , Button , H1, Wrapper } from './ctaStyle' 
+import { FormattedMessage, useIntl } from "gatsby-plugin-intl";
 import Typical from 'react-typical'
 
 
 const Cta = () => {
+    const intl = useIntl();
     const [cast, castOn] = useState(false);
+    {/*const a = <FormattedMessage id="our_content" defaultMessage={['News', 'Updates', 'Cultures', 'Business', 'Arts']}*/}
+        // {intl.formattedMessage({
+        //   id: "our_content",
+        //   defaultMessage:"['News', 'Updates', 'Cultures', 'Business', 'Arts']"
+        // })};
     return (
         <Wrapper cast={cast}>
-            <H1>We deliver{' '}
+            <H1>
+            <FormattedMessage id="deliver" defaultMessage="We deliver" />{' '}
             <Typical 
-            		steps={['News', 4000, 'Updates', 4000, 'Cultures', 4000, 'Business', 4000, 'Arts', 3500]}
+                    steps={[intl.formatMessage({id:"deliverableA"}), 4000,
+                    intl.formatMessage({id:"deliverableB"}), 4000,
+                    intl.formatMessage({id:"deliverableC"}), 4000,
+                    intl.formatMessage({id:"deliverableD"}), 4000,
+                    intl.formatMessage({id:"deliverableE"}), 3500]}
             		loop={Infinity} 
             		wrapper='span'
             		/>
             </H1>
-            <Button onClick={() => castOn(!cast)} name="cta" types="button" cast={cast}> <Pauseicon cast={cast}/>  <p>Launch Live Radio <Icon /> </p> </Button>
+            <Button onClick={() => castOn(!cast)} name="cta" types="button" cast={cast}> <Pauseicon cast={cast}/>  <p><FormattedMessage id="cta" defaultMessage="Launch Live Radio" /> <Icon /> </p> </Button>
             <Liveplayer cast={cast} />
             <P cast={cast}><span>13h-14h </span>One Hour Sport with Fedi Arfaoui</P>
         </Wrapper>
