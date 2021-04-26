76d6548a678dad458a1d42326bb2a4fdb82c2275
bassam-azizi <101010bassam@gmail.com>
2021-04-26 03:15:13 +0200

    :bowtie: add intl function and translate header elements

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,23 +5,26 @@ import Lang from "./lang"
 import Hamburger from '../../utils/hamburger'
 import Hamburger_menu from "./hamburger_menu"
 import { Wrapper, Container } from './headerStyle'
+import { useIntl } from "gatsby-plugin-intl"
 
 
 
-const Header = ({location}) => {
+
+const Header = () => {
+    const intl = useIntl();
     const [menuOpen, toggleMenu] = useState(false);
     const toggle = () => {
         toggleMenu(!menuOpen);
     }
     return (
         <Wrapper>
-            <Container>
-                <Logo />
-                <NavLinks />
-                <Hamburger toggleMenu={toggle} />
-                <Hamburger_menu menu_open={menuOpen} toggleMenu={toggle} />
-                <Lang location={location}/>
-            </Container>
+                <Container>
+                    <Logo />
+                    <NavLinks locale={intl.locale}/>
+                    <Hamburger toggleMenu={toggle} />
+                    <Hamburger_menu menu_open={menuOpen} toggleMenu={toggle} />
+                    <Lang />
+                </Container>
         </Wrapper>
     )
 }
