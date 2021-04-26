9ff70edca2c28838cc6edfd2be447b6818352b7f
bassam-azizi <101010bassam@gmail.com>
2021-04-26 03:20:59 +0200

    :bowtie: create a new schedule page that displaying the program from schedules elements

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,41 +1,165 @@
 import React from 'react'
-import { graphql , useStaticQuery } from 'gatsby'
-import { Link } from "gatsby-plugin-intl";
-import Reactmarkdown from 'react-markdown'
-import {Wrapper, Bars } from './scheduleStyle'
+import { useStaticQuery , graphql } from 'gatsby'
+import Episode from './episode'
+import { Wrapper, Container } from './schedule_style'
+
 
 
 const Schedule = () =>{
-    const data = useStaticQuery(graphql`
-    query{
-        strapiSchedule{
-            id
-            Slug
-            eng_day
-            eng_body
-          }
-    }
-    `)
-    return(
-        <Wrapper>
-            <Bars>
-                <div className="bar"></div>
-                <div className="bar"></div>
-                <div className="bar"></div>
-            </Bars>
-            <div className="container">
-                <div className="title">
-                    <h1>Today programme</h1>
-                    <p>{data.strapiSchedule.eng_day}</p>
-                </div>
-                <Reactmarkdown source={data.strapiSchedule.eng_body} />
-                <div className="footer">
-                    You can watch past shows <Link to='/emission'>here</Link>
-                </div>
-            </div>
-        </Wrapper>
-
-    )
+	const data = useStaticQuery( graphql`
+		query{
+			morning: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { day_time : { time_en : {eq: "Morning" }}}
+					){
+						nodes{
+					    	id
+						    title_en
+						    title_fr
+						    title_ar
+						    serie_en
+						    serie_fr
+						    serie_ar
+						    start_time
+						    episode_length
+						    description_en
+						    description_fr
+						    description_ar
+						    picture{
+							    childImageSharp{
+							    	fluid(maxWidth: 700){
+										...GatsbyImageSharpFluid
+							        }
+						        }
+					      	}
+					        day_time{
+					        	time_en
+					        	}
+				  			}
+				  		}
+			afterNoon: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { day_time : { time_en : {eq: "AfterNoon" }}}
+					){
+						nodes{
+					    	id
+						    title_en
+						    title_fr
+						    title_ar
+						    serie_en
+						    serie_fr
+						    serie_ar
+						    start_time
+						    episode_length
+						    description_en
+						    description_fr
+						    description_ar
+						    picture{
+							    childImageSharp{
+							    	fluid(maxWidth: 700){
+										...GatsbyImageSharpFluid
+							        }
+						        }
+					      	}
+					        day_time{
+					        	time_en
+					        	}
+					        }
+				  		}
+			evening: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { day_time : { time_en : {eq: "evening" }}}
+					){
+						nodes{
+					    	id
+						    title_en
+						    title_fr
+						    title_ar
+						    serie_en
+						    serie_fr
+						    serie_ar
+						    start_time
+						    episode_length
+						    description_en
+						    description_fr
+						    description_ar
+						    picture{
+							    childImageSharp{
+							    	fluid(maxWidth: 700){
+										...GatsbyImageSharpFluid
+							        }
+						        }
+					      	}
+					        day_time{
+					        	time_en
+					        	}
+					        }
+				  		}
+			late: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { day_time : { time_en : {eq: "late" }}}
+					){
+						nodes{
+					    	id
+						    title_en
+						    title_fr
+						    title_ar
+						    serie_en
+						    serie_fr
+						    serie_ar
+						    start_time
+						    episode_length
+						    description_en
+						    description_fr
+						    description_ar
+						    picture{
+							    childImageSharp{
+							    	fluid(maxWidth: 700){
+										...GatsbyImageSharpFluid
+							        }
+						        }
+					      	}
+					        day_time{
+					        	time_en
+					        	}
+					        }
+				  		}
+			}
+	`)
+	const schedule_render = (schedules,title) =>{
+		return (
+			<Container>
+				<h1 className="time_title">{title}</h1>
+				<div className="time_schedules">
+					{schedules.map(node=>
+										<Episode data={node} />
+									)}
+				</div>
+			</Container>
+	)};
+	return(
+		<Wrapper>
+			
+			{data.morning.nodes.length>0?
+									schedule_render(data.morning.nodes, "Morning")				
+									:
+									null
+			}{data.afterNoon.nodes.length>0?
+									schedule_render(data.afterNoon.nodes, "After noon")				
+									:
+									null
+			}{data.evening.nodes.length>0?
+									schedule_render(data.evening.nodes, "Evening")				
+									:
+									null
+			}{data.late.nodes.length>0?
+									schedule_render(data.late.nodes, "Late")				
+									:
+									null
+			}
+			
+		</Wrapper>
+	)
 }
 
-export default Schedule 
+export default Schedule
