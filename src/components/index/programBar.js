85e84a7aec7258f82d7780f8b0fe53fcf2a3fcf1
bassam-azizi <101010bassam@gmail.com>
2021-04-26 03:19:36 +0200

    :bowtie: change the structure of programBar and use program element from schedule elements

--- src/components/index/programBar.js
+++ src/components/index/programBar.js
@@ -1,50 +1,83 @@
 import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
-import Reactmarkdown from 'react-markdown'
+// import Reactmarkdown from 'react-markdown'
 import {keyframes} from '@emotion/react'
 import Styled from '@emotion/styled'
 
 
 const movingText = keyframes`
     0%{margin-left: 100%;}
-    100%{margin-left: -300% ;}
+    100%{margin-left: -180% ;}
 `
 const Wrapper = Styled.div`
     width: 100%;
-    background-color: #51965722;
+    // background-color: #51965722;
     font-size: 14px;
     font-weight: 200;
     color: #373737;
-    ul{
-        animation: ${movingText} 45s linear infinite;
-        // margin-left: 100%;
+
+    .container{
+        overflow: hidden;
         display: flex;
-        flex-wrap: nowrap;
         align-items: center;
         height: 106px;
-        overflow: hidden;
-        li{
-            padding-right: 250px;
-            white-space: nowrap;
-            letter-spacing: .3px;
+        animation: ${movingText} 40s linear infinite;
+    }
+
+    .schedule-element{
+        display: flex;
+        margin: 0 130px;
+        white-space: nowrap;
+        letter-spacing: .3px;
+        h4{
+            padding: 0 3px;
         }
     }
     `
+    // ul{
+    //     animation: ${movingText} 45s linear infinite;
+    //     // margin-left: 100%;
+    //     display: flex;
+    //     flex-wrap: nowrap;
+    //     align-items: center;
+    //     height: 106px;
+    //     overflow: hidden;
+    //     li{
+    //         padding-right: 250px;
+    //         white-space: nowrap;
+    //         letter-spacing: .3px;
+    //     }
+    // }
 
 const ProgramBar = () =>{
     const data = useStaticQuery(graphql`
-    query{
-        strapiSchedule{
-            id
-            Slug
-            eng_day
-            eng_body
-          }
-    }
+        query{
+            allStrapiSchedule(
+                sort:{ fields : [start_time] , order: ASC }
+            ){
+                edges{
+                    node{
+                        id
+                        title_en
+                        title_fr
+                        title_ar
+                        start_time
+                    }
+                }
+            }
+        }
     `)
     return(
         <Wrapper>
-            <Reactmarkdown source={data.strapiSchedule.eng_body} />
+            <div className="container">
+                {data.allStrapiSchedule.edges.map(edge=>
+                    <div className="schedule-element" key={edge.node.id}>
+                        <h4>{edge.node.start_time} - </h4>
+                        <h4>{edge.node.title_en}</h4>
+                    </div>
+                    )
+                }
+            </div>
         </Wrapper>
     )
 } 
