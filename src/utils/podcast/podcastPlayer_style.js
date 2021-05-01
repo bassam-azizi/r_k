623920e17ce857aeb90daca9374affd540be3a6b
bassam-azizi <101010bassam@gmail.com>
2021-05-01 06:31:14 +0200

    :bowtie: throw podcast and podcast_GlobalStyle to the utils folder and make it isolated for reusability

---
+++ src/utils/podcast/podcastPlayer_style.js
@@ -0,0 +1,34 @@
+import Styled from '@emotion/styled'
+
+export const Wrapper = Styled.div`
+    background-color:  #aaa;
+    margin: 48px 20px 48px 0;
+    position: relative;
+
+`
+export const Info = Styled.div`
+    position: relative;
+    margin-left: 15px;
+    text-align: left;
+    width: 200px;
+    overflow: hidden;
+        h5{
+            line-height: 18.75px;
+            font-weight: 400;
+            font-size: 16px;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+        }
+        p{
+            position: absolute;
+            font-weight: 200px;
+            font-size: 12px;
+            line-height: 14px;
+            color: #919191;
+        }  
+
+        @media (max-width: 1300px){
+            width: 60%;
+        } 
+ `
