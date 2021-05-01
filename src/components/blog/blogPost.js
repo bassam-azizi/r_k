d2c4e9419ad38dde4cca22a44c8d2cc3c1d5500e
bassam-azizi <101010bassam@gmail.com>
2021-05-01 06:13:58 +0200

    :bowtie: structuring and cleaning

--- src/components/blog/blogPost.js
+++ src/components/blog/blogPost.js
@@ -6,15 +6,15 @@ import {Wrapper, Picture, Title, H1, Lin} from './blogPostStyle'
 
 
 
-const Blogpost = props =>{
+const Blogpost = ({data}) =>{
     return(
-            <Wrapper key={props.data.node.Slug}>
-                <Lin to={`/blog/${props.data.node.Slug}`}>
+            <Wrapper key={data.node.Slug}>
+                <Lin to={`/blog/${data.node.Slug}`}>
                     <Picture>
-                        <Img fixed={props.data.node.featuredImg.childImageSharp.fixed} loading="lazy" alt={props.data.node.name} />
+                        <Img fixed={data.node.featuredImg.childImageSharp.fixed} loading="lazy" alt={data.node.name} />
                     </Picture>  
                     <Title>
-                        <H1>{props.data.node.name}</H1>
+                        <H1>{data.node.name}</H1>
                     </Title>
                 </Lin>
             </Wrapper>
