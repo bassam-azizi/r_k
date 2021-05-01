a91e464d5adbf72b58ff4da9dd79d90ee1fcf38b
bassam-azizi <101010bassam@gmail.com>
2021-05-01 06:25:53 +0200

    :bowtie: create a new component Episode

---
+++ src/components/episodes/episode.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import {Wrapper} from './episode_style'
+import Img from 'gatsby-image'
+
+
+
+const Episode = ({data}) =>{
+	return(
+		<Wrapper key={data.node.id}>
+			<Link to={`/episodes/${data.node.$slug}`}>
+				<div className="pic">
+					<Img fluid={data.node.picture.childImageSharp.fluid} alt={data.node.title_en} />
+				</div>
+				<div className="header">
+					<h1>{data.node.title_en}</h1>
+					<span className="serie">{data.node.serie_en}</span>
+					<p className="description">{data.node.description_en}</p>
+				</div>
+			</Link>
+		</Wrapper>
+		)
+}
+
+export default Episode
