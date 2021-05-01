3ec531d119c3f261da1d8645de26fbc8a319175b
bassam-azizi <101010bassam@gmail.com>
2021-05-01 06:33:49 +0200

    :bowtie: throw Timer component and Today_is component in the utils folder and make them isolated for reusability

---
+++ src/utils/timer.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+
+import Styled from '@emotion/styled'
+
+const Time = Styled.div`
+    font-family:  Poppins;
+    font-weight: 200;
+    font-size: 14px;
+    padding: 0 15px;
+    opacity: ${(props) => (props.cast? '0' : '1')};
+    transition: all 4s ease-in;
+`
+
+const Timer = () =>{
+    const [date, currentTime] = useState(new Date())
+    useEffect(()=>{
+        const id = setInterval( () =>{
+            currentTime(() => new Date())
+        }, 1000);
+        return () => clearInterval(id);
+    });
+        return (
+            <Time>{date.toLocaleTimeString('it-IT')}</Time>
+        )
+}
+
+export default Timer
---
+++ src/utils/todayIs.js
@@ -0,0 +1,10 @@
+
+
+export const Today_is = () =>{
+	let date = new Date();
+	return(
+		" " + date.toLocaleDateString('en-UK',{weekday:'long', day:'numeric', month:'numeric', year:'numeric'})
+		// date.toLocaleDateString('ar-ma',{weekday:'long', day:'numeric', month:'numeric', year:'numeric'});
+		// date.toLocaleDateString('fr-FR',{weekday:'long', day:'numeric', month:'numeric', year:'numeric'});
+		)
+}
