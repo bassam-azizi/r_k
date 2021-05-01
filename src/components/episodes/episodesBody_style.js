13f052e87eaf6f5fd05a6401a18a17b1ecd45d39
bassam-azizi <101010bassam@gmail.com>
2021-05-01 06:26:59 +0200

    :bowtie: style episodeBody and set it to a grid layout

---
+++ src/components/episodes/episodesBody_style.js
@@ -0,0 +1,12 @@
+import Styled from '@emotion/styled'
+
+
+
+export const Wrapper = Styled.div`
+	width: 87%;
+	max-width: 1320px;
+	margin: 90px auto;
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 70px;
+`
