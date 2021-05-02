647dddc3ef8de5cf61fd959d7be8f6b00d252f13
bassam-azizi <101010bassam@gmail.com>
2021-05-02 04:18:32 +0200

    :bowtie:  give a color, font-size and margin to today title

--- src/components/schedule/schedulesBody_style.js
+++ src/components/schedule/schedulesBody_style.js
@@ -5,13 +5,19 @@ export const Wrapper = Styled.div`
 	font-size: 16px;
 	width: 80%;
 	margin: 107px auto;
+
+	.today{
+		font-size: 16px;
+		color: #83836D;
+		margin: 70px 20px;
+	}
 `
 export const Container = Styled.div`
 	display: flex;
 	justify-content: center;
 
 	.time_title{
-		color: #7F8A64;
+		color: #83836D;
 		font-size: 17px;
 		position: relative;
 		padding-right: 70px;
