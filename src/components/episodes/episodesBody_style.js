a854f05512c0c19c9b5a4aa0127ae2abb49552b8
bassam-azizi <101010bassam@gmail.com>
2021-05-11 21:34:28 +0200

    :bowtie: setting wrapper's max width to 1270px

--- src/components/episodes/episodesBody_style.js
+++ src/components/episodes/episodesBody_style.js
@@ -4,7 +4,7 @@ import Styled from '@emotion/styled'
 
 export const Wrapper = Styled.div`
 	width: 87%;
-	max-width: 1320px;
+	max-width: 1270px;
 	margin: 90px auto;
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
