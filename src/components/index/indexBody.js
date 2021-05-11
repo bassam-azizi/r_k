4966b641cb32d8cecf2fd6c414038942f418daa3
bassam-azizi <101010bassam@gmail.com>
2021-05-11 21:42:20 +0200

    :bowtie: set the index body from flex box to grid layout and adjust it's rules

--- src/components/index/indexBody.js
+++ src/components/index/indexBody.js
@@ -10,13 +10,26 @@ const Wrapper = Styled.div`
     justify-content: flex-end;
 `
 const Container = Styled.div`
-    display: flex;
-    // margin-top: 64px;
-    justify-content: flex-end;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    // display: flex;
+    // // margin-top: 64px;
+    // justify-content: flex-end;
 
-    @media (max-width: 1028px) {
-        flex-direction: column;
-        justify-content: center;
+    @media (max-width: 1350px){
+        grid-template-columns: repeat(11,1fr);
+    }
+
+    @media (max-width: 1108px) {
+        grid-template-columns: 50px repeat(11, 1fr);
+        // flex-direction: column;
+        // justify-content: center;
+    }
+    @media (max-width: 916px){
+        grid-template-columns: 110px repeat(11, 1fr);
+    }
+    @media (max-width: 768px){
+        grid-template-columns: 50px repeat(11, 1fr);
     }
 `
 
