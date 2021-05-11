1f04ae4353663830c00c4992bf8bf02ac4e5b6fd
bassam-azizi <101010bassam@gmail.com>
2021-05-11 21:39:29 +0200

    :bowtie: restructure the jsx for convienience grid layout

--- src/components/index/cta/cta.js
+++ src/components/index/cta/cta.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Liveplayer from './livePlayer'
-import { P , Pauseicon , Icon , Button , H1, Wrapper } from './ctaStyle' 
+import { P , Pauseicon , Icon , Button , H1, BtnWrapper , Container, Wrapper } from './ctaStyle' 
 import { FormattedMessage, useIntl } from "gatsby-plugin-intl";
 import Typical from 'react-typical'
 
@@ -15,21 +15,25 @@ const Cta = () => {
         // })};
     return (
         <Wrapper cast={cast}>
-            <H1 locale={intl.locale}>
-                <FormattedMessage id="deliver" defaultMessage="We deliver" />{' '}
-                <Typical 
-                        steps={[intl.formatMessage({id:"deliverableA"}), 4000,
-                        intl.formatMessage({id:"deliverableB"}), 4000,
-                        intl.formatMessage({id:"deliverableC"}), 4000,
-                        intl.formatMessage({id:"deliverableD"}), 4000,
-                        intl.formatMessage({id:"deliverableE"}), 3500]}
-                		loop={Infinity} 
-                		wrapper='span'
-                		/>
-            </H1>
-            <Button locale={intl.locale} onClick={() => castOn(!cast)} name="cta" types="button" cast={cast}> <Pauseicon cast={cast}/>  <p><FormattedMessage id="cta" defaultMessage="Launch Live Radio" /> <Icon locale={intl.locale}/> </p> </Button>
-            <Liveplayer cast={cast} />
-            <P cast={cast}><span>13h-14h </span>One Hour Sport with Fedi Arfaoui</P>
+            <Container cast={cast} locale={intl.locale}>
+                <H1 locale={intl.locale} cast={cast}>
+                    <FormattedMessage id="deliver" defaultMessage="We deliver " />{' '}
+                    <Typical 
+                            steps={[intl.formatMessage({id:"deliverableA"}), 4000,
+                            intl.formatMessage({id:"deliverableB"}), 4000,
+                            intl.formatMessage({id:"deliverableC"}), 4000,
+                            intl.formatMessage({id:"deliverableD"}), 4000,
+                            intl.formatMessage({id:"deliverableE"}), 3500]}
+                    		loop={Infinity} 
+                    		wrapper='span'
+                    		/>
+                </H1>
+                <BtnWrapper>
+                    <Button locale={intl.locale} onClick={() => castOn(!cast)} name="cta" types="button" cast={cast}> <Pauseicon cast={cast}/>  <p><FormattedMessage id="cta" defaultMessage="Launch Live Radio" /> <Icon locale={intl.locale}/> </p> </Button>
+                    <Liveplayer cast={cast} />
+                    <P cast={cast}><span>13h-14h </span>One Hour Sport with Fedi Arfaoui</P>
+                </BtnWrapper>
+            </Container>
         </Wrapper>
     )
 }
