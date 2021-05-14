371989bfe2d369493e4b4dd493e104618a5b3d4e
bassam-azizi <101010bassam@gmail.com>
2021-05-14 21:18:40 +0200

    :bowtie: set the gird container params on the episodesBody

--- src/components/episodes/episodesBody_style.js
+++ src/components/episodes/episodesBody_style.js
@@ -3,10 +3,29 @@ import Styled from '@emotion/styled'
 
 
 export const Wrapper = Styled.div`
-	width: 87%;
-	max-width: 1270px;
-	margin: 90px auto;
+	// width: 87%;
+	// max-width: 1270px;
+	margin: 90px 0;
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
-	grid-gap: 70px;
+	grid-template-columns: repeat(12, 1fr);
+
+	@media (max-width: 1100px){
+            grid-template-columns: 50px repeat(12, 1fr) 50px;
+        }
+    @media (max-width: 916px){
+        grid-template-columns: 110px repeat(12, 1fr) 110px;
+    }
+    @media (max-width: 768px){
+        grid-template-columns: 50px repeat(12, 1fr) 50px;
+    }
+    @media(max-width: 420px){
+    	grid-template-columns: repeat(12,1fr);
+    }
 `
+
+export const Container = Styled.div`
+	grid-column: 2/-2;
+	display: grid;
+	grid-template-columns: repeat(auto-fit,minmax(240px, 1fr));
+	grid-gap: 35px;
+	`
