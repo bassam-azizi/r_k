1b678a47c4760ea1efe3106c2aedd9d0ab7725b8
bassam-azizi <101010bassam@gmail.com>
2021-05-18 00:27:38 +0200

    :bowtie: gave the mrgin top of every wrapper a 90px and adjust content size for different screen size

--- src/components/blog/blogBody.js
+++ src/components/blog/blogBody.js
@@ -9,7 +9,7 @@ import Blogpost from './blogPost'
 const Wrapper = Styled.div`
     display: grid;
     grid-template-columns: repeat(12, 1fr);
-    margin: 71px 0 101px;
+    margin: 90px 0 101px;
 
     @media (max-width: 1100px){
             grid-template-columns: 50px repeat(12, 1fr) 50px;
@@ -17,7 +17,7 @@ const Wrapper = Styled.div`
     @media (max-width: 916px){
         grid-template-columns: 110px repeat(12, 1fr) 110px;
     }
-    @media (max-width: 768px){
+    @media (max-width: 857px){
         grid-template-columns: 50px repeat(12, 1fr) 50px;
     }
     @media(max-width: 420px){
@@ -29,6 +29,13 @@ const Container = Styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-gap: 35px;
+
+    @media (max-width: 660px){
+        grid-column: 3/-3;
+    }
+    @media (max-width: 550px){
+        grid-column: 2/-2;
+    }
 `
     
 
