c4620dfc730c34738fdf4553e48c3d7d291c967a
bassam-azizi <101010bassam@gmail.com>
2021-05-27 21:23:59 +0200

    :bowtie: apply the new gatsby-plugin-image rules on schedule

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Img from 'gatsby-image'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Wrapper } from './schedule_style'
 
 
 
 
 const Schedule = ({data}) =>{
-	let episode_path = data.title_en.toLowerCase() ;
+	let episode_path = data.title.toLowerCase() ;
+	let image = getImage(data.picture.localFile) ;
 	return(
 			<Wrapper key={data.id}>
 				<Link to={`/schedule/${episode_path}`}>
-				{console.log(data.title_en.toLowerCase())}
 					<div className="image-container">
-						<Img fluid={data.picture.childImageSharp.fluid} alt={data.title_en}/>
+						<GatsbyImage image={image} alt={data.title} />
 					</div>
 					<div className="info-container">
 						<div className="header">
 							<div className="title">
-								<h1>{data.title_en}</h1>
-								<p>{data.serie_en} </p>
+								<h1>{data.title}</h1>
+								<p>{data.serie} </p>
 							</div>
 							<div className="start-time">
 								<h2>{data.start_time}</h2>
 							</div>
 						</div>
 						<div className="description">
-							<p>{data.description_en}</p>
+							<p>{data.description}</p>
 						</div>
 					</div>
 				</Link>
