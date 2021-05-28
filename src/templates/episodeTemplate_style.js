5a6e99a30ca1122195631b700179d9c6b8e7be34
bassam-azizi <101010bassam@gmail.com>
2021-05-28 20:25:25 +0200

    :bowtie: center the image and styled adjust to the other elements

--- src/templates/episodeTemplate_style.js
+++ src/templates/episodeTemplate_style.js
@@ -7,13 +7,19 @@ export const Wrapper = Styled.div`
 	.image{
 		width: 100%;
 		margin: 20px 0 ;
+		display: grid;
+		justify-content: center;
+
+		.gatsby-image-wrapper{
+			border-radius: 5px;
+		}
 	}
 
 	.header{
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
-		margin: 20px 0;
+		margin: 40px 0 5px;
 
 		.info_one{
 			h1{
@@ -21,26 +27,40 @@ export const Wrapper = Styled.div`
 				font-size: 24px;
 			}
 			p{
-				font-style: italic;
-				color: #222;
-				font-style: 18px;
+				color: #444;
+				font-size: 14px;
 			}
 		}
 		.info_two{
 			h1{
 				color:#7F8A64;
-				font-size: 24px;
+				font-size: 18px;
+				font-weight: bold;
 			}
 			p{
-				font-style: italic;
-				color: #222;
-				font-style: 18px;
+				color: #666;
+				font-style: 14px;
 			}
 		}
 	}
 
+	.podcast{
+		display: grid;
+		justify-content: center;
+	}
+
 	.description{
-		color: #222;
+		color: rgb(51, 51, 51);
+
+		p{
+			margin: 36px 0;
+			letter-spacing: 1.007px;
+			font-size: 17px;
+			line-height: 44px;
+			font-weight: 400;
+			text-indent: 2px;
+			outline: currentcolor none medium;
+		}
 	}
 
 `
