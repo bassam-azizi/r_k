723fde13620ec72d03bcfde4743e777c53f8bba0
bassam-azizi <101010bassam@gmail.com>
2021-05-30 06:03:49 +0200

    :bowtie: try commenting the ssr-config page code to test deployment error

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
-var React = require("react");
+// var React = require("react");
 
-exports.onPreRenderHTML = ({getHeadComponents, replaceHeadComponents }) => {
-    /**
-     * @type {any[]} headComponents
-     */
-    const headComponents = getHeadComponents();
+// exports.onPreRenderHTML = ({getHeadComponents, replaceHeadComponents }) => {
+//     /**
+//      * @type {any[]} headComponents
+//      */
+//     const headComponents = getHeadComponents();
 
-    headComponents.sort((a, b) => {
-        if (a.props && a.props["data-react-helmet"]) {
-            return 0;
-        }
-        return 1;
-    });
-    replaceHeadComponents(headComponents);
-};
+//     headComponents.sort((a, b) => {
+//         if (a.props && a.props["data-react-helmet"]) {
+//             return 0;
+//         }
+//         return 1;
+//     });
+//     replaceHeadComponents(headComponents);
+// };
