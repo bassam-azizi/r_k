14d480cdb8ab05130d89ef04e24378c8f449e1c1
bassam-azizi <101010bassam@gmail.com>
2021-06-01 04:48:04 +0200

    :bowtie: use oldfashion/graphql alias to have dynamic content loading by locales

--- src/components/schedule/schedulesBody.js
+++ src/components/schedule/schedulesBody.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStaticQuery , graphql } from 'gatsby'
+import { useIntl } from "gatsby-plugin-intl"
 import Schedule from './schedule'
 import {TodayIs} from '../../utils/todayIs'
 import { Wrapper, Container } from './schedulesBody_style'
@@ -9,10 +10,13 @@ import { Wrapper, Container } from './schedulesBody_style'
 const SchedulesBody = () =>{
 	const data = useStaticQuery( graphql`
 		query{
-			morning: allStrapiSchedule(
+			enMorning: allStrapiSchedule(
 					sort:{ fields : [start_time] , order: ASC }
-				    filter: { day_time : { time : {eq: "Morning" }}}
-					){
+				    filter: { 
+				    	day_time : { time : {eq: "Morning" }},
+				    	locale : { eq: "en"}
+				    	}
+				    ){
 						nodes{
 					    	id
 						    title
@@ -35,9 +39,70 @@ const SchedulesBody = () =>{
 					        	}
 				  			}
 				  		}
-			afterNoon: allStrapiSchedule(
+			frMorning: allStrapiSchedule(
 					sort:{ fields : [start_time] , order: ASC }
-				    filter: { day_time : { time : {eq: "AfterNoon" }}}
+				    filter: { 
+				    	day_time : { time : {eq: "Morning" }},
+				    	locale : { eq: "fr"}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+			arMorning: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Morning" }},
+				    	locale : { eq: "ar"}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+			enAfterNoon: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "afterNoon" }},
+				    	locale : { eq: "en"}
+				    	}
 					){
 						nodes{
 					    	id
@@ -61,9 +126,12 @@ const SchedulesBody = () =>{
 					        	}
 					        }
 				  		}
-			evening: allStrapiSchedule(
+			frAfterNoon: allStrapiSchedule(
 					sort:{ fields : [start_time] , order: ASC }
-				    filter: { day_time : { time : {eq: "evening" }}}
+				    filter: { 
+				    	day_time : { time : {eq: "afterNoon" }},
+				    	locale : { eq: "fr"}
+				    	}
 					){
 						nodes{
 					    	id
@@ -87,9 +155,186 @@ const SchedulesBody = () =>{
 					        	}
 					        }
 				  		}
-			late: allStrapiSchedule(
+			arAfterNoon: allStrapiSchedule(
 					sort:{ fields : [start_time] , order: ASC }
-				    filter: { day_time : { time : {eq: "late" }}}
+				    filter: { 
+				    	day_time : { time : {eq: "Noon" }},
+				    	locale : { eq: "ar"}
+				    	}
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			enEvening: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: {
+				    	 day_time : { time : {eq: "Noon" }},
+				    	 locale : { eq: "en"}
+				    	 }
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			frEvening: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: {
+				    	 day_time : { time : {eq: "Noon" }},
+				    	 locale : { eq: "fr"}
+				    	 }
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			arEvening: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: {
+				    	 day_time : { time : {eq: "Noon" }},
+				    	 locale : { eq: "ar"}
+				    	 }
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			enLate: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Night" }},
+				    	locale : { eq: "en"}
+				    	}
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			frLate: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Night" }},
+				    	locale : { eq: "fr"}
+				    	}
+					){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+					        }
+				  		}
+			arLate: allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Night" }},
+				    	locale : { eq: "ar"}
+				    	}
 					){
 						nodes{
 					    	id
@@ -126,23 +371,67 @@ const SchedulesBody = () =>{
 				</div>
 			</Container>
 	)};
+	const morning = ( locale) =>{
+		switch(locale){
+			case("fr"):
+				return data.frMorning;
+			case("ar"):
+				return data.arMorning;
+			default:
+				return data.enMorning;
+		}
+	}
+	const afterNoon = ( locale) =>{
+		switch(locale){
+			case("fr"):
+				return data.frAfterNoon;
+			case("ar"):
+				return data.arAfterNoon;
+			default:
+				return data.enAfterNoon;
+		}
+	}
+	const evening = ( locale) =>{
+		switch(locale){
+			case("fr"):
+				return data.frEvening;
+			case("ar"):
+				return data.arEvening;
+			default:
+				return data.enEvening;
+		}
+	}
+	const late = ( locale) =>{
+		console.log(locale);
+		switch(locale){
+			case("fr"):
+				return data.frLate;
+			case("ar"):
+				return data.arLate;
+			default:
+				return data.enLate;
+		}
+	}
+	const currentLocale = useIntl().locale ;
 	return(
 		<Wrapper>
 			<h1 className="today">Today is <TodayIs /></h1>
-			{data.morning.nodes.length>0?
-									schedule_render(data.morning.nodes, "Morning")				
+			{morning(currentLocale).nodes.length>0?
+									schedule_render(morning(currentLocale).nodes, "Morning")				
 									:
 									null
-			}{data.afterNoon.nodes.length>0?
-									schedule_render(data.afterNoon.nodes, "After noon")				
+			}
+			{afterNoon(currentLocale).nodes.length>0?
+									schedule_render(afterNoon(currentLocale).nodes, "After noon")				
 									:
 									null
-			}{data.evening.nodes.length>0?
-									schedule_render(data.evening.nodes, "Evening")				
+			}
+			{evening(currentLocale).nodes.length>0?
+									schedule_render(evening(currentLocale).nodes, "Evening")				
 									:
 									null
-			}{data.late.nodes.length>0?
-									schedule_render(data.late.nodes, "Late")				
+			}{late(currentLocale).nodes.length>0?
+									schedule_render(late(currentLocale).nodes, "Late")				
 									:
 									null
 			}
