409870e9085b883eb172b8557e51a761a36174f2
bassam-azizi <101010bassam@gmail.com>
2021-06-01 04:47:13 +0200

    :bowtie: use oldfashion/graphql alias to have dynamic content loading by locales

--- src/components/episodes/episodesBody.js
+++ src/components/episodes/episodesBody.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
+import { useIntl } from "gatsby-plugin-intl"
 import { Wrapper, Container } from './episodesBody_style'
 import Episode from './episode'
 
@@ -7,7 +8,76 @@ import Episode from './episode'
 const EpisodesBody = () =>{
 	const data = useStaticQuery(graphql`
 		query{
-			allStrapiEpisode{
+			enEpisodes : allStrapiEpisode(
+				sort: { order: DESC, fields: [published_at] }
+                limit: 1000
+                filter: {
+                    locale: { eq : "en"}
+                }
+			){
+				edges{
+					node{
+						id
+						slug
+						title
+						serie
+						description
+						picture{
+							localFile{
+								childImageSharp{
+									gatsbyImageData(
+										layout: CONSTRAINED
+									)
+								}
+							}
+						}
+						
+						author{
+							name
+						}
+						published_at(formatString:"MM/D/YYYY")
+					}
+				}
+			}
+			frEpisodes : allStrapiEpisode(
+				sort: { order: DESC, fields: [published_at] }
+                limit: 1000
+                filter: {
+                    locale: { eq : "fr"}
+                }
+			){
+			
+				edges{
+					node{
+						id
+						slug
+						title
+						serie
+						description
+						picture{
+							localFile{
+								childImageSharp{
+									gatsbyImageData(
+										layout: CONSTRAINED
+									)
+								}
+							}
+						}
+						
+						author{
+							name
+						}
+						published_at(formatString:"MM/D/YYYY")
+					}
+				}
+			}
+			arEpisodes : allStrapiEpisode(
+				sort: { order: DESC, fields: [published_at] }
+                limit: 1000
+                filter: {
+                    locale: { eq : "ar"}
+                }
+			){
 				edges{
 					node{
 						id
@@ -33,13 +103,25 @@ const EpisodesBody = () =>{
 				}
 			}
 			
+			
 
 		}
 	`)
+	const strapiEpisodes = (locale) =>{
+            switch(locale){
+                case("fr"):
+                    return data.frEpisodes;
+                case("ar"):
+                    return data.arEpisodes;
+                default:
+                    return data.enEpisodes;
+            }
+
+        }
 	return(
 		<Wrapper>
 			<Container>
-				{data.allStrapiEpisode.edges.map(edge=>
+				{strapiEpisodes(useIntl().locale).edges.map(edge=>
 						<Episode data={edge} />
 					)}
 			</Container>
