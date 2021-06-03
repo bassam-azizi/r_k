c0487c060b7d222d5d5866b28ed634ca1e753ae7
bassam-azizi <101010bassam@gmail.com>
2021-06-03 22:21:54 +0200

    :bowtie: remove the old fashion alias query fo languages sourced content and use the context loaded from parent page in the filter query

--- src/pages/program.js
+++ src/pages/program.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
@@ -7,13 +8,134 @@ import SchedulesBody from '../components/schedule/schedulesBody'
 
 
 
-const Program = () =>{
+const Program = (data) =>{
     return (
         <Layout> 
         	<Head title="Our Schedule" />
-            <SchedulesBody />
+            <SchedulesBody data={data}/>
         </Layout>
     )
 }
 
 export default Program
+
+export const pageQuery = graphql`
+        query($locale: String!){
+            morning : allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Morning" }},
+				    	locale : { eq: $locale}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+			noon : allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Noon" }},
+				    	locale : { eq: $locale}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+			afterNoon : allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Afternoon" }},
+				    	locale : { eq: $locale}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+			night : allStrapiSchedule(
+					sort:{ fields : [start_time] , order: ASC }
+				    filter: { 
+				    	day_time : { time : {eq: "Night" }},
+				    	locale : { eq: $locale}
+				    	}
+				    ){
+						nodes{
+					    	id
+						    title
+						    serie
+						    start_time
+						    episode_length
+						    description
+						    picture{
+						    	localFile{
+						    		childImageSharp{
+						    			gatsbyImageData(
+						    				layout: FULL_WIDTH
+						    			)
+						    		}
+						    	}
+						    }
+						    
+					        day_time{
+					        	time
+					        	}
+				  			}
+				  		}
+            }
+    `
