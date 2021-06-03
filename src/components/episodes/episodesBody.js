44fa7fb24a98f8279950e4e03cb268ae2509a7cc
bassam-azizi <101010bassam@gmail.com>
2021-06-03 22:20:35 +0200

    :bowtie: remove the old fashion alias query fo languages sourced content and use the context loaded from parent page in the filter query

--- src/components/episodes/episodesBody.js
+++ src/components/episodes/episodesBody.js
@@ -1,128 +1,16 @@
 import React from 'react'
-import {useStaticQuery, graphql} from 'gatsby'
-import { useIntl } from "gatsby-plugin-intl"
 import { Wrapper, Container } from './episodesBody_style'
 import Episode from './episode'
 
 
-const EpisodesBody = () =>{
-	const data = useStaticQuery(graphql`
-		query{
-			enEpisodes : allStrapiEpisode(
-				sort: { order: DESC, fields: [published_at] }
-                limit: 1000
-                filter: {
-                    locale: { eq : "en"}
-                }
-			){
-				edges{
-					node{
-						id
-						slug
-						title
-						serie
-						description
-						picture{
-							localFile{
-								childImageSharp{
-									gatsbyImageData(
-										layout: CONSTRAINED
-									)
-								}
-							}
-						}
-						
-						author{
-							name
-						}
-						published_at(formatString:"MM/D/YYYY")
-					}
-				}
-			}
-			frEpisodes : allStrapiEpisode(
-				sort: { order: DESC, fields: [published_at] }
-                limit: 1000
-                filter: {
-                    locale: { eq : "fr"}
-                }
-			){
-			
-				edges{
-					node{
-						id
-						slug
-						title
-						serie
-						description
-						picture{
-							localFile{
-								childImageSharp{
-									gatsbyImageData(
-										layout: CONSTRAINED
-									)
-								}
-							}
-						}
-						
-						author{
-							name
-						}
-						published_at(formatString:"MM/D/YYYY")
-					}
-				}
-			}
-			arEpisodes : allStrapiEpisode(
-				sort: { order: DESC, fields: [published_at] }
-                limit: 1000
-                filter: {
-                    locale: { eq : "ar"}
-                }
-			){
-				edges{
-					node{
-						id
-						slug
-						title
-						serie
-						description
-						picture{
-							localFile{
-								childImageSharp{
-									gatsbyImageData(
-										layout: CONSTRAINED
-									)
-								}
-							}
-						}
-						
-						author{
-							name
-						}
-						published_at(formatString:"MM/D/YYYY")
-					}
-				}
-			}
-			
-			
-
-		}
-	`)
-	const strapiEpisodes = (locale) =>{
-            switch(locale){
-                case("fr"):
-                    return data.frEpisodes;
-                case("ar"):
-                    return data.arEpisodes;
-                default:
-                    return data.enEpisodes;
-            }
-
-        }
+const EpisodesBody = ({data}) =>{
+console.log(data);
+	const episodes = data.data.allStrapiEpisode;
 	return(
 		<Wrapper>
 			<Container>
-				{strapiEpisodes(useIntl().locale).edges.map(edge=>
-						<Episode data={edge} />
+				{episodes.edges.map(edge=>
+						<Episode data={edge} key={edge.node.id}/>
 					)}
 			</Container>
 		</Wrapper>
