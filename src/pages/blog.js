d162f6fed24db7dc02e61eaae20359cc4433efa3
bassam-azizi <101010bassam@gmail.com>
2021-06-03 22:21:25 +0200

    :bowtie: remove the old fashion alias query fo languages sourced content and use the context loaded from parent page in the filter query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 
 import Layout from '../components/layout'
 import Head from '../components/head'
@@ -6,13 +8,45 @@ import Head from '../components/head'
 
 import Bloga from '../components/blog/blogBody'
 
-const Blog = () => {
+const Blog = (data) => {
     return (
         <Layout>
         	<Head title="Press" />
-            <Bloga />
+
+            <Bloga data={data}/>
         </Layout>
     )
 }
 
 export default Blog
+
+export const pageQuery = graphql`
+        query($locale: String!){
+            allStrapiBlogpost(
+                sort: { order: DESC, fields: [pubdate] }
+                limit: 1000
+                filter: {
+                    locale: { eq : $locale}
+                }
+              ){
+                edges{
+                    node{
+                        name
+                        Slug
+                        id
+                        pubdate(formatString:"MM/D/YYYY")
+                        featuredImg{
+                            localFile{
+                                childImageSharp{
+                                    gatsbyImageData(
+                                        layout: FULL_WIDTH
+                                    )
+                                }
+                            }
+                        }
+                        body
+                    }
+                }
+            }
+            }
+    `
