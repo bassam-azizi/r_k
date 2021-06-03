6a2898dbcf268fa2f8ce3761fa698c364701b48d
bassam-azizi <101010bassam@gmail.com>
2021-06-03 22:21:36 +0200

    :bowtie: remove the old fashion alias query fo languages sourced content and use the context loaded from parent page in the filter query

--- src/pages/emission.js
+++ src/pages/emission.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 
 import EpisodesBody from '../components/episodes/episodesBody'
 
@@ -8,15 +10,51 @@ import Head from '../components/head'
 
 
 
-const Emission = () =>{
+const Emission = (data) =>{
 	// const url = typeof window !== 'undefined' ? window.location.path : '/emission';
     return (
         <Layout>
       		<Head title="past emission"></Head>
-            <EpisodesBody  />
+            <EpisodesBody  data={data}/>
         </Layout>
 
     )
 }
 
 export default Emission
+
+export const pageQuery = graphql`
+        query($locale: String!){
+            allStrapiEpisode(
+				sort: { order: DESC, fields: [published_at] }
+                limit: 1000
+                filter: {
+                    locale: { eq : $locale}
+                }
+			){
+				edges{
+					node{
+						id
+						slug
+						title
+						serie
+						description
+						picture{
+							localFile{
+								childImageSharp{
+									gatsbyImageData(
+										layout: CONSTRAINED
+									)
+								}
+							}
+						}
+						
+						author{
+							name
+						}
+						published_at(formatString:"MM/D/YYYY")
+					}
+				}
+			}
+		}
+    `
