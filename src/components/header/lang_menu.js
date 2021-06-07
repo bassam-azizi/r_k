74d3228a523c8128ac22878efd104e529f26cff7
bassam-azizi <101010bassam@gmail.com>
2021-06-07 05:17:55 +0200

    :bowtie: apply dynamic language translation to the menu language

--- src/components/header/lang_menu.js
+++ src/components/header/lang_menu.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import { Wrapper, Close, Lin} from './style_lang_menu'
+import { IntlContextConsumer, changeLocale } from "gatsby-plugin-intl"
+import { Wrapper, Close, Span} from './style_lang_menu'
 
+
+
+const languageName = {
+  en: "En",
+  fr: "Fr",
+  ar: "Ar",
+}
 
 const LangMenu = props =>{
     return(
         <Wrapper menu_open={props.menu_open}>
-            <Close role="button" onClick={props.toggle} onKeyDown={props.toggle} tabIndex={0}>^</Close>
-            <Lin to="/">English</Lin>
-            <Lin to="/">Francais</Lin>
-            <Lin to="/">العربية</Lin>
+	            <Close role="button" onClick={props.toggle} onKeyDown={props.toggle} tabIndex={0}>^</Close>
+	            <IntlContextConsumer>
+                    {  ({ languages, language: currentLocale }) =>
+                          languages.filter(language => (language!==currentLocale)).map(language => (
+                            <Span
+                              key={language}
+                              role = "button"
+                              onClick={() => changeLocale(language)}
+                              onKeyDown={() => changeLocale(language)}
+                              tabIndex={0}
+                            >
+                                {languageName[language]}
+                            </Span>
+                             ))
+                    }
+                  </IntlContextConsumer>
         </Wrapper>
     )
 }
