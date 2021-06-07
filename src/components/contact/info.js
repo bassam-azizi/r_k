57c6a9860b35afa2011f09476edccf68a4fa822a
bassam-azizi <101010bassam@gmail.com>
2021-06-07 05:13:04 +0200

    :bowtie: content fixing errors

--- src/components/contact/info.js
+++ src/components/contact/info.js
@@ -7,14 +7,14 @@ const Info = () =>{
         <Wrapper>
             <div className="adress">
                 <h1>Radio Web Khemir</h1>
-                <p>Maison des jeunes, Ain Draham_8730</p>
+                <p>Maison des jeunes, Ain Draham_8130</p>
                 <p>Tel:  00216 41 425 865</p>
                 <p>Fax: 00216 71 428 965</p>
             </div>
             <div className="sponsored">
                 <p>Fondeé par L’association</p>
                 <p><bold>Femme Pour Developpment AFPD</bold></p>
-                <p>Financeé par le <bold>GIZ</bold></p>
+                <p>Financeé par le <bold>l'ambassade d'allemagne en tunisie</bold></p>
             </div>
         </Wrapper>
     )
